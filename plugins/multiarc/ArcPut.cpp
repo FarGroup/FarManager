5c1a086cd9efc774dcf83fce5d7b5a64cbe8594a
Ivan Sintyurin <spinoza@mail.ru>
2002-07-31 00:00:01 +0000

    Multiarc patch 00127.GoToFile Дата              : 31.07.2002 Сделал            : Ivan Sintyurin Описание          : - баг: неправильное позиционирование курсора на созданный                       архив (это я успешно чинил в 102 и поломал в 105 :-) Измененные файлы  : ArcMix.cpp ArcPut.cpp MultiArc.hpp Новые файлы       : Удаленные файлы   : Состав            : 00127.GoToFile.txt                     ArcMix.cpp.127.diff                     ArcPut.cpp.127.diff                     MultiArc.hpp.127.diff Основан на патче  : 126 Дополнение        :
    бонус патча: позиционируем курсор руками только, если созданный архив
    находится в текущем каталоге (чтобы резко не менялась панель, что уже
    успело надоесть после 102)

--- plugins/multiarc/ArcPut.cpp
+++ plugins/multiarc/ArcPut.cpp
@@ -655,7 +655,8 @@ int PluginClass::PutFiles(struct PluginPanelItem *PanelItem,int ItemsNumber,
   if (Opt.UpdateDescriptions && ArcExitCode)
     for (int I=0;I<ItemsNumber;I++)
       PanelItem[I].Flags|=PPIF_PROCESSDESCR;
-  if(!Opt.UserBackground && ArcExitCode && NewArchive && GoToFile(ArcName))
+  if(!Opt.UserBackground && ArcExitCode && NewArchive &&
+     GoToFile(ArcName, FALSE))
     ArcExitCode=2;
   return ArcExitCode;
 }
