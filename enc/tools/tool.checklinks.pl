bc1ad2c52c72385041d6940a3485207c96797894
Valentin Skirdin <vskirdin@gmail.com>
2005-10-13 11:48:46 +0000

    change tool.checklinks.pl

--- enc/tools/tool.checklinks.pl
+++ enc/tools/tool.checklinks.pl
@@ -2,16 +2,19 @@
 #######################################
 # проверяет ссылки по всем html-ям
 
-$dir = "../meta";
-
+$dir = "../enc_rus/meta";
 srch($dir);
+
+$dir = "../enc_eng/meta";
+srch($dir);
+
 
 sub srch
 {
  local($dr1) = @_[0];
  local($dr)=$dr1."/";
- printf "$dr1\n";
- foreach(`ls --ignore=CVS $dr1`){
+ #printf "$dr1\n";
+ foreach(`ls --ignore=.svn $dr1`){
    chomp;
    if (-d $dr.$_){
      srch("$dr$_");
@@ -25,7 +28,9 @@ sub srch
        if (/(href|src)=["'] ([^\'\"\#]+?) [\'\"\#]/xi){
          $ff = $2;
          unless($ff=~/http|mailto|ftp|news/ || -r $dr.$ff){
-           print "! $dr$fn - $ff\n";
+           if(!($ff =~ /win32\//)){
+             print "! $dr$fn - $ff\n";
+           };
          };
        };
      };
