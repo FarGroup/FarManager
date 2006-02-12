8280dbe2b9005eaf3ad9972340a64ab6d79ec827
Alex Yaroslavsky <trexinc@gmail.com>
2006-02-12 17:38:01 +0000

    update crt.hpp

--- plugins/common/crt.hpp
+++ plugins/common/crt.hpp
@@ -1,6 +1,16 @@
 #ifndef __CRT_HPP__
 #define __CRT_HPP__
 
+#include "./CRT/delete.hpp"
+#include "./CRT/delete_array.hpp"
+#include "./CRT/free.hpp"
+#include "./CRT/malloc.hpp"
+#include "./CRT/new.hpp"
+#include "./CRT/new_array.hpp"
+#include "./CRT/realloc.hpp"
+
+#if defined(__GNUC__)
+
 #include "./CRT/ctype.hpp"
 #include "./CRT/memchr.hpp"
 #include "./CRT/memcmp.hpp"
@@ -12,12 +22,7 @@
 #include "./CRT/strrchr.hpp"
 #include "./CRT/strstr.hpp"
 #include "./CRT/strtok.hpp"
-#include "./CRT/delete.hpp"
-#include "./CRT/delete_array.hpp"
-#include "./CRT/free.hpp"
-#include "./CRT/malloc.hpp"
-#include "./CRT/new.hpp"
-#include "./CRT/new_array.hpp"
-#include "./CRT/realloc.hpp"
+
+#endif
 
 #endif
