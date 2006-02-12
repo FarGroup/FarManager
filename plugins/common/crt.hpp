60121e1da65e5211bd7b0ac82edc30f7819145db
Alex Yaroslavsky <trexinc@gmail.com>
2006-02-12 17:09:04 +0000

    update structure

---
+++ plugins/common/crt.hpp
@@ -0,0 +1,23 @@
+#ifndef __CRT_HPP__
+#define __CRT_HPP__
+
+#include "./CRT/ctype.hpp"
+#include "./CRT/memchr.hpp"
+#include "./CRT/memcmp.hpp"
+#include "./CRT/memcpy.hpp"
+#include "./CRT/memmove.hpp"
+#include "./CRT/memset.hpp"
+#include "./CRT/strchr.hpp"
+#include "./CRT/strpbrk.hpp"
+#include "./CRT/strrchr.hpp"
+#include "./CRT/strstr.hpp"
+#include "./CRT/strtok.hpp"
+#include "./CRT/delete.hpp"
+#include "./CRT/delete_array.hpp"
+#include "./CRT/free.hpp"
+#include "./CRT/malloc.hpp"
+#include "./CRT/new.hpp"
+#include "./CRT/new_array.hpp"
+#include "./CRT/realloc.hpp"
+
+#endif
