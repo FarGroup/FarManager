21d6e76ca1f2d7a617049b7aaaef66675d93cf7e
Alex Yaroslavsky <trexinc@gmail.com>
2006-02-14 00:38:46 +0000

    gcc bug fixes

--- plugins/brackets/BrackCfg.cpp
+++ plugins/brackets/BrackCfg.cpp
@@ -1,26 +1,26 @@
 int Config()
 {
   static struct InitDialogItem InitItems[]={
-  /* 0*/  DI_DOUBLEBOX,3,1,57,19,0,0,0,0,(char *)MTitle,
+  /* 0*/  {DI_DOUBLEBOX,3,1,57,19,0,0,0,0,(char *)MTitle},
 
-  /* 1*/  DI_SINGLEBOX,5,2,55,8,0,0,DIF_LEFTTEXT,0,(char *)MRules,
-  /* 2*/  DI_CHECKBOX,7,3,0,0,0,0,0,0,(char*)MIgnoreQuotation,
-  /* 3*/  DI_CHECKBOX,7,4,0,0,0,0,0,0,(char*)MIgnoreAfter,
-  /* 4*/  DI_CHECKBOX,7,5,0,0,0,0,0,0,(char*)MPriority,
-  /* 5*/  DI_CHECKBOX,7,6,0,0,0,0,0,0,(char*)MJumpToPair,
-  /* 6*/  DI_CHECKBOX,7,7,0,0,0,0,0,0,(char*)MBeep,
+  /* 1*/  {DI_SINGLEBOX,5,2,55,8,0,0,DIF_LEFTTEXT,0,(char *)MRules},
+  /* 2*/  {DI_CHECKBOX,7,3,0,0,0,0,0,0,(char*)MIgnoreQuotation},
+  /* 3*/  {DI_CHECKBOX,7,4,0,0,0,0,0,0,(char*)MIgnoreAfter},
+  /* 4*/  {DI_CHECKBOX,7,5,0,0,0,0,0,0,(char*)MPriority},
+  /* 5*/  {DI_CHECKBOX,7,6,0,0,0,0,0,0,(char*)MJumpToPair},
+  /* 6*/  {DI_CHECKBOX,7,7,0,0,0,0,0,0,(char*)MBeep},
 
-  /* 7*/  DI_SINGLEBOX,5,9,55,16,0,0,DIF_LEFTTEXT,0,(char *)MDescriptions,
-  /* 8*/  DI_TEXT,7,10,0,0,0,0,0,0,(char*)MTypeQuotes,
-  /* 9*/  DI_FIXEDIT,7,11,26,0,0,0,0,0,"",
-  /*10*/  DI_TEXT,7,12,0,0,0,0,0,0,(char*)MDescript1,
-  /*11*/  DI_FIXEDIT,7,13,26,0,0,0,0,0,"",
-  /*12*/  DI_TEXT,7,14,0,0,0,0,0,0,(char*)MDescript2,
-  /*13*/  DI_FIXEDIT,7,15,46,0,0,0,0,0,"",
+  /* 7*/  {DI_SINGLEBOX,5,9,55,16,0,0,DIF_LEFTTEXT,0,(char *)MDescriptions},
+  /* 8*/  {DI_TEXT,7,10,0,0,0,0,0,0,(char*)MTypeQuotes},
+  /* 9*/  {DI_FIXEDIT,7,11,26,0,0,0,0,0,""},
+  /*10*/  {DI_TEXT,7,12,0,0,0,0,0,0,(char*)MDescript1},
+  /*11*/  {DI_FIXEDIT,7,13,26,0,0,0,0,0,""},
+  /*12*/  {DI_TEXT,7,14,0,0,0,0,0,0,(char*)MDescript2},
+  /*13*/  {DI_FIXEDIT,7,15,46,0,0,0,0,0,""},
 
-  /*14*/  DI_TEXT,-1,17,0,0,0,0,DIF_SEPARATOR,0,"",
-  /*15*/  DI_BUTTON,0,18,0,0,0,0,DIF_CENTERGROUP,1,(char *)MSave,
-  /*16*/  DI_BUTTON,0,18,0,0,0,0,DIF_CENTERGROUP,0,(char *)MCancel
+  /*14*/  {DI_TEXT,-1,17,0,0,0,0,DIF_SEPARATOR,0,""},
+  /*15*/  {DI_BUTTON,0,18,0,0,0,0,DIF_CENTERGROUP,1,(char *)MSave},
+  /*16*/  {DI_BUTTON,0,18,0,0,0,0,DIF_CENTERGROUP,0,(char *)MCancel}
   };
 
   struct FarDialogItem DialogItems[sizeof(InitItems)/sizeof(InitItems[0])];
