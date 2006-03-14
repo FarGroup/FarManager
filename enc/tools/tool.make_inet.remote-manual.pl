21c0ea67179f0cd54e498e10c518ccce46d1a5fc
Alex Yaroslavsky <trexinc@gmail.com>
2006-03-14 20:45:38 +0000

    add scripts for remote site update

---
+++ enc/tools/tool.make_inet.remote-manual.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/perl -w
+$dest_dr            = "/var/www/api";
+
+$dest_dr_inet       = $dest_dr."/temp";
+
+print "PREPARING INET PROJECT\n";
+
+print "\n  -- clear INET\n";
+
+system "rm -Rf ".$dest_dr."/*";
+
+print "\n  -- making directories tree\n";
+
+mkdir $dest_dr_inet, 0775;
+mkdir $dest_dr."/images", 0775;
+mkdir $dest_dr."/styles", 0775;
+
+mk_inet_lng("ru","rus");
+mk_inet_lng("en","eng");
+
+system "svn export -q --force --username=fargroup --password=secret svn://localhost/svn/enc/tools/trunk/inet/ ".$dest_dr_inet."/inet";
+system "cp -f ".$dest_dr_inet."/inet/index.html ".$dest_dr;
+system "cp -f ".$dest_dr_inet."/inet/farenclogo.gif ".$dest_dr."/images/farenclogo.gif";
+system "cp -f ".$dest_dr_inet."/inet/styles.css ".$dest_dr."/styles/styles.css";
+
+system "rm -Rf ".$dest_dr_inet;
+
+system "chown -R :webadmins ".$dest_dr."/*";
+
+sub mk_inet_lng
+{
+  local($dr1) = $_[0];
+  local($dr2) = $_[1];
+
+  print " -- preparing ".$dr1." --\n";
+  system "svn export -q --force --username=fargroup --password=secret svn://localhost/svn/enc/meta_".$dr2."/trunk ".$dest_dr_inet."/meta.".$dr1;
+  mkdir $dest_dr."/".$dr1."/", 0775;
+  system "cp -Rf ".$dest_dr_inet."/meta.".$dr1."/meta/* ".$dest_dr."/".$dr1;
+  system "cp -Rf ".$dest_dr_inet."/meta.".$dr1."/images/* ".$dest_dr."/images";
+  system "rm -Rf ".$dest_dr_inet."/meta.".$dr1;
+  $lev = -1;
+  fix($dest_dr."/".$dr1);
+};
+
+sub fix
+{
+ $lev++;
+
+ local($dr1) = $_[0];
+ local($dr)=$dr1."/";
+ printf "$dr\n";
+
+ foreach(`ls -1 $dr1`){
+   chomp;
+   if (-d $dr.$_) {
+     fix("$dr$_");
+   }
+
+   if (/\.html$/){
+     open F, $dr.$_;
+     @FData = <F>;
+     close F;
+
+     open F, ">$dr$_";
+     foreach(@FData){
+       #if (/<meta\s.*?Windows-1251.*$/xi){
+       #  next;
+       #};
+       $back = "../"x$lev;
+       s[ <a \s* href=\"win32\/ .*?>(.*?)</a> ][ <a href=\"${back}notfound.html?$1\">$1</a> ]igx;
+       print F $_;
+     };
+     close F;
+   };
+ };
+ $lev--;
+};
