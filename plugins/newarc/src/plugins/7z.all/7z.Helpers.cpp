31fa256b5dfa9f009658d5fb252afd34ddc963fa
WARP ItSelf <warpitself@gmail.com>
2006-07-02 20:29:40 +0000

    code format

--- plugins/newarc/src/plugins/7z.all/7z.Helpers.cpp
+++ plugins/newarc/src/plugins/7z.all/7z.Helpers.cpp
@@ -2,180 +2,180 @@
 
 bool CInFile::Open (const char *lpFileName)
 {
-  HANDLE hFile = CreateFile (
-      lpFileName,
-      GENERIC_READ,
-      FILE_SHARE_READ,
-      NULL,
-      OPEN_EXISTING,
-      FILE_FLAG_SEQUENTIAL_SCAN,
-      NULL
-      );
+	HANDLE hFile = CreateFile (
+			lpFileName,
+			GENERIC_READ,
+			FILE_SHARE_READ,
+			NULL,
+			OPEN_EXISTING,
+			FILE_FLAG_SEQUENTIAL_SCAN,
+			NULL
+			);
 
-  if ( hFile != INVALID_HANDLE_VALUE )
-  {
-    m_hFile = hFile;
-    return true;
-  }
+	if ( hFile != INVALID_HANDLE_VALUE )
+	{
+		m_hFile = hFile;
+		return true;
+	}
 
-  return false;
+	return false;
 }
 
 CInFile::CInFile ()
 {
-  m_nRefCount = 1;
-  m_hFile = INVALID_HANDLE_VALUE;
+	m_nRefCount = 1;
+	m_hFile = INVALID_HANDLE_VALUE;
 }
 
 CInFile::~CInFile ()
 {
-  if ( m_hFile != INVALID_HANDLE_VALUE )
-    CloseHandle (m_hFile);
+	if ( m_hFile != INVALID_HANDLE_VALUE )
+		CloseHandle (m_hFile);
 }
 
 
 
 HRESULT __stdcall CInFile::QueryInterface (const IID &iid, void ** ppvObject)
 {
-  *ppvObject = NULL;
+	*ppvObject = NULL;
 
-  if ( iid == IID_IInStream )
-  {
-    *ppvObject = this;
-    AddRef ();
+	if ( iid == IID_IInStream )
+	{
+		*ppvObject = this;
+		AddRef ();
 
-    return S_OK;
-  }
+		return S_OK;
+	}
 
-  return E_NOINTERFACE;
+	return E_NOINTERFACE;
 }
 
 ULONG __stdcall CInFile::AddRef ()
 {
-  return ++m_nRefCount;
+	return ++m_nRefCount;
 }
 
 ULONG __stdcall CInFile::Release ()
 {
-  if ( --m_nRefCount == 0 )
-  {
-    delete this;
-    return 0;
-  }
+	if ( --m_nRefCount == 0 )
+	{
+		delete this;
+		return 0;
+	}
 
-  return m_nRefCount;
+	return m_nRefCount;
 }
 
 
 
 HRESULT __stdcall CInFile::Read (void *data, unsigned int size, unsigned int *processedSize)
 {
-  return ReadFile (m_hFile, data, size, (LPDWORD)processedSize, NULL)?S_OK:E_FAIL;
+	return ReadFile (m_hFile, data, size, (LPDWORD)processedSize, NULL)?S_OK:E_FAIL;
 }
 
 HRESULT __stdcall CInFile::Seek (__int64 offset, unsigned int seekOrigin, unsigned __int64 *newPosition)
 {
-  DWORD hi, lo;
+	DWORD hi, lo;
 
-  hi = offset >> 32;
-  lo = (DWORD)offset;
+	hi = offset >> 32;
+	lo = (DWORD)offset;
 
-  lo = SetFilePointer (m_hFile, lo, (PLONG)&hi, seekOrigin);
+	lo = SetFilePointer (m_hFile, lo, (PLONG)&hi, seekOrigin);
 
-  if ( (lo == INVALID_SET_FILE_POINTER) && (GetLastError () != NO_ERROR) )
-    return E_FAIL;
-  else
-  {
-    if ( newPosition )
-      *newPosition = ((unsigned __int64)hi)*0x100000000ll+(unsigned __int64)lo;
-    return S_OK;
-  }
+	if ( (lo == INVALID_SET_FILE_POINTER) && (GetLastError () != NO_ERROR) )
+		return E_FAIL;
+	else
+	{
+		if ( newPosition )
+			*newPosition = ((unsigned __int64)hi)*0x100000000ll+(unsigned __int64)lo;
+		return S_OK;
+	}
 }
 
 
 CArchiveExtractCallback::CArchiveExtractCallback (
-    SevenZipArchive *pArchive,
-    ArchiveItem *pItems,
-    int nItemsNumber,
-    const char *lpDestPath,
-    const char *lpCurrentFolder
-    )
+		SevenZipArchive *pArchive,
+		ArchiveItem *pItems,
+		int nItemsNumber,
+		const char *lpDestPath,
+		const char *lpCurrentFolder
+		)
 {
-  m_pArchive = pArchive;
-  m_nRefCount = 1;
+	m_pArchive = pArchive;
+	m_nRefCount = 1;
 
-  m_pItems = pItems;
-  m_nItemsNumber = nItemsNumber;
+	m_pItems = pItems;
+	m_nItemsNumber = nItemsNumber;
 
-  m_lpDestPath = StrDuplicate (lpDestPath);
-  m_lpCurrentFolder = StrDuplicate (lpCurrentFolder);
+	m_lpDestPath = StrDuplicate (lpDestPath);
+	m_lpCurrentFolder = StrDuplicate (lpCurrentFolder);
 
-  FSF.AddEndSlash (m_lpDestPath);
-  FSF.AddEndSlash (m_lpCurrentFolder);
+	FSF.AddEndSlash (m_lpDestPath);
+	FSF.AddEndSlash (m_lpCurrentFolder);
 
-  m_nLastProcessed = -1;
+	m_nLastProcessed = -1;
 }
 
 
 CArchiveExtractCallback::~CArchiveExtractCallback ()
 {
-  StrFree (m_lpDestPath);
-  StrFree (m_lpCurrentFolder);
+	StrFree (m_lpDestPath);
+	StrFree (m_lpCurrentFolder);
 }
 
 
 
 ULONG __stdcall CArchiveExtractCallback::AddRef ()
 {
-  return ++m_nRefCount;
+	return ++m_nRefCount;
 }
 
 ULONG __stdcall CArchiveExtractCallback::Release ()
 {
-  if ( --m_nRefCount == 0 )
-  {
-    delete this;
-    return 0;
-  }
+	if ( --m_nRefCount == 0 )
+	{
+		delete this;
+		return 0;
+	}
 
-  return m_nRefCount;
+	return m_nRefCount;
 }
 
 
 HRESULT __stdcall CArchiveExtractCallback::QueryInterface (const IID &iid, void ** ppvObject)
 {
-  *ppvObject = NULL;
+	*ppvObject = NULL;
 
-  if ( iid == IID_IArchiveExtractCallback )
-  {
-    *ppvObject = this;
-    AddRef ();
+	if ( iid == IID_IArchiveExtractCallback )
+	{
+		*ppvObject = this;
+		AddRef ();
 
-    return S_OK;
-  }
+		return S_OK;
+	}
 
-  return E_NOINTERFACE;
+	return E_NOINTERFACE;
 }
 
 
 HRESULT __stdcall CArchiveExtractCallback::SetTotal (unsigned __int64 total)
 {
-  m_nLastProcessed = -1;
-  return S_OK;
+	m_nLastProcessed = -1;
+	return S_OK;
 }
 
 HRESULT CArchiveExtractCallback::SetCompleted (const unsigned __int64* completeValue)
 {
-  if ( m_nLastProcessed != -1 )
-  {
-    unsigned __int64 diff = *completeValue-m_nLastProcessed;
+	if ( m_nLastProcessed != -1 )
+	{
+		unsigned __int64 diff = *completeValue-m_nLastProcessed;
 
-    m_pArchive->m_pfnCallback (AM_PROCESS_DATA, 0, (int)diff);
+		m_pArchive->m_pfnCallback (AM_PROCESS_DATA, 0, (int)diff);
 
-    m_nLastProcessed = *completeValue;
-  }
+		m_nLastProcessed = *completeValue;
+	}
 
-  return S_OK;
+	return S_OK;
 }
 
 
@@ -204,239 +204,238 @@ void CreateDirectory(char *FullPath) //$ 16.05.2002 AA
 
 void CreateDirs (const char *lpFileName)
 {
-  char *lpNameCopy = StrDuplicate (lpFileName);
+	char *lpNameCopy = StrDuplicate (lpFileName);
 
-  CutToSlash (lpNameCopy);
+	CutToSlash (lpNameCopy);
 
-  CreateDirectory (lpNameCopy);
+	CreateDirectory (lpNameCopy);
 
-  StrFree (lpNameCopy);
+	StrFree (lpNameCopy);
 }
 
 
 int GetItemIndex (CArchiveExtractCallback *pcb, int index)
 {
-  for (int i = 0; i < pcb->m_nItemsNumber; i++)
-  {
-    if ( pcb->m_pItems[i].nIndex == index )
-      return i;
-  }
+	for (int i = 0; i < pcb->m_nItemsNumber; i++)
+	{
+		if ( pcb->m_pItems[i].nIndex == index )
+			return i;
+	}
 
-  return -1;
+	return -1;
 }
 
 
 
 HRESULT __stdcall CArchiveExtractCallback::GetStream (
-    unsigned int index,
-    ISequentialOutStream **outStream,
-    int askExtractMode
-    )
+		unsigned int index,
+		ISequentialOutStream **outStream,
+		int askExtractMode
+		)
 
 {
-  PROPVARIANT value;
+	PROPVARIANT value;
 
-  IInArchive *archive = m_pArchive->m_pArchive;
-  VariantInit ((VARIANTARG*)&value);
+	IInArchive *archive = m_pArchive->m_pArchive;
+	VariantInit ((VARIANTARG*)&value);
 
-  if ( askExtractMode == 0 ) //extract
-  {
-    if ( SUCCEEDED (archive->GetProperty (
-        index,
-        kpidPath,
-        &value
-        )) )
-    {
-      char szArcFileName[NM];
-      char szFullName[NM];
+	if ( askExtractMode == 0 ) //extract
+	{
+		if ( SUCCEEDED (archive->GetProperty (
+				index,
+				kpidPath,
+				&value
+				)) )
+		{
+			char szArcFileName[NM];
+			char szFullName[NM];
 
-      if ( value.vt == VT_BSTR )
-        WideCharToMultiByte (CP_OEMCP, 0, value.bstrVal, -1, szArcFileName, NM, NULL, NULL);
-      else
-      {
-        strcpy (szArcFileName, FSF.PointToName (m_pArchive->m_lpFileName));
-        CutTo (szArcFileName, '.', true);
+			if ( value.vt == VT_BSTR )
+				WideCharToMultiByte (CP_OEMCP, 0, value.bstrVal, -1, szArcFileName, NM, NULL, NULL);
+			else
+			{
+				strcpy (szArcFileName, FSF.PointToName (m_pArchive->m_lpFileName));
+				CutTo (szArcFileName, '.', true);
+			}
 
-      }
+			strcpy (szFullName, m_lpDestPath);
 
-      strcpy (szFullName, m_lpDestPath);
+			FSF.AddEndSlash (szFullName);
 
-      FSF.AddEndSlash (szFullName);
+			if ( !FSF.LStrnicmp (szArcFileName, m_lpCurrentFolder, strlen (m_lpCurrentFolder)) )
+				strcat (szFullName, szArcFileName+strlen (m_lpCurrentFolder));
+			else
+				strcat (szFullName, szArcFileName);
 
-      if ( !FSF.LStrnicmp (szArcFileName, m_lpCurrentFolder, strlen (m_lpCurrentFolder)) )
-        strcat (szFullName, szArcFileName+strlen (m_lpCurrentFolder));
-      else
-        strcat (szFullName, szArcFileName);
+			int itemindex = GetItemIndex (this, index);
+			PluginPanelItem *item = m_pItems[itemindex].pItem;
 
-      int itemindex = GetItemIndex (this, index);
-      PluginPanelItem *item = m_pItems[itemindex].pItem;
+			m_pArchive->m_pfnCallback (AM_START_EXTRACT_FILE, (int)item, (int)szFullName);
 
-      m_pArchive->m_pfnCallback (AM_START_EXTRACT_FILE, (int)item, (int)szFullName);
+			if ( m_nLastProcessed == -1 )
+				m_nLastProcessed = 0;
 
-      if ( m_nLastProcessed == -1 )
-        m_nLastProcessed = 0;
+			CreateDirs (szFullName);
 
-      CreateDirs (szFullName);
+			COutFile *file = new COutFile;
+			file->Open (szFullName);
 
-      COutFile *file = new COutFile;
-      file->Open (szFullName);
+			*outStream = file;
+			VariantClear ((VARIANTARG*)&value);
+		}
+	}
+	else
+		*outStream = NULL;
 
-      *outStream = file;
-      VariantClear ((VARIANTARG*)&value);
-    }
-  }
-  else
-    *outStream = NULL;
-
-  return S_OK;
+	return S_OK;
 }
 
 HRESULT __stdcall CArchiveExtractCallback::PrepareOperation (int askExtractMode)
 {
-  return S_OK;
+	return S_OK;
 }
 
 HRESULT __stdcall CArchiveExtractCallback::SetOperationResult (int resultEOperationResult)
 {
-  return S_OK;
+	return S_OK;
 }
 
 
 bool COutFile::Open (const char *lpFileName)
 {
-  HANDLE hFile = CreateFile (
-      lpFileName,
-      GENERIC_WRITE,
-      FILE_SHARE_READ,
-      NULL,
-      CREATE_ALWAYS,
-      FILE_FLAG_SEQUENTIAL_SCAN,
-      NULL
-      );
+	HANDLE hFile = CreateFile (
+			lpFileName,
+			GENERIC_WRITE,
+			FILE_SHARE_READ,
+			NULL,
+			CREATE_ALWAYS,
+			FILE_FLAG_SEQUENTIAL_SCAN,
+			NULL
+			);
 
-  if ( hFile != INVALID_HANDLE_VALUE )
-  {
-    m_hFile = hFile;
-    return true;
-  }
+	if ( hFile != INVALID_HANDLE_VALUE )
+	{
+		m_hFile = hFile;
+		return true;
+	}
 
-  return false;
+	return false;
 }
 
 COutFile::COutFile ()
 {
-  m_nRefCount = 1;
-  m_hFile = INVALID_HANDLE_VALUE;
+	m_nRefCount = 1;
+	m_hFile = INVALID_HANDLE_VALUE;
 }
 
 COutFile::~COutFile ()
 {
-  if ( m_hFile != INVALID_HANDLE_VALUE )
-    CloseHandle (m_hFile);
+	if ( m_hFile != INVALID_HANDLE_VALUE )
+		CloseHandle (m_hFile);
 }
 
 
 HRESULT __stdcall COutFile::QueryInterface (const IID &iid, void ** ppvObject)
 {
-  *ppvObject = NULL;
+	*ppvObject = NULL;
 
-  if ( iid == IID_ISequentialOutStream )
-  {
-    *ppvObject = this;
-    AddRef ();
+	if ( iid == IID_ISequentialOutStream )
+	{
+		*ppvObject = this;
+		AddRef ();
 
-    return S_OK;
-  }
+		return S_OK;
+	}
 
-  return E_NOINTERFACE;
+	return E_NOINTERFACE;
 }
 
 ULONG __stdcall COutFile::AddRef ()
 {
-  return ++m_nRefCount;
+	return ++m_nRefCount;
 }
 
 ULONG __stdcall COutFile::Release ()
 {
-  if ( --m_nRefCount == 0 )
-  {
-    delete this;
-    return 0;
-  }
+	if ( --m_nRefCount == 0 )
+	{
+		delete this;
+		return 0;
+	}
 
-  return m_nRefCount;
+	return m_nRefCount;
 }
 
 
 HRESULT __stdcall COutFile::Write (const void *data, unsigned int size, unsigned int *processedSize)
 {
-  return WriteFile (m_hFile, data, size, (LPDWORD)processedSize, NULL)?S_OK:E_FAIL;
+	return WriteFile (m_hFile, data, size, (LPDWORD)processedSize, NULL)?S_OK:E_FAIL;
 }
 
 
 ULONG __stdcall CArchiveOpenCallback::AddRef ()
 {
-  return ++m_nRefCount;
+	return ++m_nRefCount;
 }
 
 ULONG __stdcall CArchiveOpenCallback::Release ()
 {
-  if ( --m_nRefCount == 0 )
-  {
-    delete this;
-    return 0;
-  }
+	if ( --m_nRefCount == 0 )
+	{
+		delete this;
+		return 0;
+	}
 
-  return m_nRefCount;
+	return m_nRefCount;
 }
 
 
 HRESULT __stdcall CArchiveOpenCallback::QueryInterface (const IID &iid, void ** ppvObject)
 {
-  *ppvObject = NULL;
+	*ppvObject = NULL;
 
-  if ( iid == IID_IArchiveOpenCallback )
-  {
-    *ppvObject = this;
-    AddRef ();
+	if ( iid == IID_IArchiveOpenCallback )
+	{
+		*ppvObject = this;
+		AddRef ();
 
-    return S_OK;
-  }
+		return S_OK;
+	}
 
-  if ( iid == IID_IArchiveOpenVolumeCallback )
-  {
-    *ppvObject = this;
-    AddRef ();
+	if ( iid == IID_IArchiveOpenVolumeCallback )
+	{
+		*ppvObject = this;
+		AddRef ();
 
-    return S_OK;
-  }
+		return S_OK;
+	}
 
 
-  return E_NOINTERFACE;
+	return E_NOINTERFACE;
 }
 
 CArchiveOpenCallback::CArchiveOpenCallback ()
 {
-  m_nRefCount = 1;
+	m_nRefCount = 1;
 }
 
 HRESULT __stdcall CArchiveOpenCallback::SetTotal (const UInt64 *files, const UInt64 *bytes)
 {
-  return S_OK;
+	return S_OK;
 }
 
 HRESULT __stdcall CArchiveOpenCallback::SetCompleted (const UInt64 *files, const UInt64 *bytes)
 {
-  return S_OK;
+	return S_OK;
 }
 
 
 HRESULT __stdcall CArchiveOpenCallback::GetProperty (PROPID propID, PROPVARIANT *value)
 {
-  return S_OK;
+	return S_OK;
 }
 
 HRESULT __stdcall CArchiveOpenCallback::GetStream (const wchar_t *name, IInStream **inStream)
 {
-  return S_OK;
+	return S_OK;
 }
