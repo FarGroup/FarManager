eed98e2b87e44636f135f2cc0c3ed01060decbd2
WARP ItSelf <warpitself@gmail.com>
2006-12-05 00:44:54 +0000

    IsModulePlugin & small fixes

--- far/plugins.cpp
+++ far/plugins.cpp
@@ -173,7 +173,6 @@ void PluginsSet::SendExit()
   }
 }
 
-
 void PluginsSet::LoadPlugins()
 {
   BlockExtKey blockExtKey;
@@ -238,7 +237,8 @@ void PluginsSet::LoadPlugins()
       // ...и пройдемс€ по нему
       while (ScTree.GetNextNameW(&FindData,strFullName))
       {
-        if (CmpNameW(L"*.dll",FindData.strFileName,FALSE) && (FindData.dwFileAttributes & FA_DIREC)==0)
+        if ( CmpNameW(L"*.dll",FindData.strFileName,FALSE) && 
+             (FindData.dwFileAttributes & FA_DIREC)==0 )
         {
           PluginItem *CurPlugin = new PluginItem;
           string strRegKey;
@@ -291,6 +291,8 @@ void PluginsSet::LoadPlugins()
             PluginsData[PluginsCount]=CurPlugin;
             PluginsCount++;
           }
+          else
+          	delete CurPlugin;
         }
       } // end while
     }
@@ -443,6 +445,170 @@ static BOOL PrepareModulePathW(const wchar_t *ModuleName)
   return FarChDirW(strModulePath,TRUE);
 }
 
+
+unsigned long CRC32(
+    unsigned long crc,
+    const char *buf,
+    unsigned int len
+    )
+{
+  static unsigned long crc_table[256];
+
+  if (!crc_table[1])
+  {
+    unsigned long c;
+    int n, k;
+
+    for (n = 0; n < 256; n++)
+    {
+      c = (unsigned long)n;
+      for (k = 0; k < 8; k++) c = (c >> 1) ^ (c & 1 ? 0xedb88320L : 0);
+        crc_table[n] = c;
+    }
+  }
+
+  crc = crc ^ 0xffffffffL;
+  while (len-- > 0) {
+    crc = crc_table[(crc ^ (*buf++)) & 0xff] ^ (crc >> 8);
+  }
+
+  return crc ^ 0xffffffffL;
+}
+
+
+#define CRC32_SETSTARTUPINFO  0xF537107A
+#define CRC32_GETPLUGININFO   0xDB6424B4
+#define CRC32_OPENPLUGIN    0x601AEDE8
+#define CRC32_OPENFILEPLUGIN  0xAC9FF5CD
+#define CRC32_EXITFAR     0x04419715
+#define CRC32_SETFINDLIST   0x7A74A2E5
+#define CRC32_CONFIGURE     0x4DC1BC1A
+#define CRC32_GETMINFARVERSION  0x2BBAD952
+
+DWORD ExportCRC32[7] = {
+    CRC32_SETSTARTUPINFO,
+    CRC32_GETPLUGININFO,
+    CRC32_OPENPLUGIN,
+    CRC32_OPENFILEPLUGIN,
+    CRC32_EXITFAR,
+    CRC32_SETFINDLIST,
+//    CRC32_CONFIGURE,
+    CRC32_GETMINFARVERSION
+    };
+
+BOOL IsModulePlugin2 (
+	PBYTE hModule
+	)
+{
+	DWORD dwExportAddr;
+
+	PIMAGE_DOS_HEADER pDOSHeader = (PIMAGE_DOS_HEADER)hModule;
+	PIMAGE_NT_HEADERS pPEHeader;
+
+	__try {
+
+		if ( pDOSHeader->e_magic != IMAGE_DOS_SIGNATURE )
+			return FALSE;
+
+		pPEHeader = (PIMAGE_NT_HEADERS)&hModule[pDOSHeader->e_lfanew];
+
+		if ( pPEHeader->Signature != IMAGE_NT_SIGNATURE )
+			return FALSE;
+
+		if ( (pPEHeader->FileHeader.Characteristics & IMAGE_FILE_DLL) == 0 )
+			return FALSE;
+
+		dwExportAddr = pPEHeader->OptionalHeader.DataDirectory[0].VirtualAddress;
+
+		if ( !dwExportAddr )
+			return FALSE;
+
+		PIMAGE_SECTION_HEADER pSection = (PIMAGE_SECTION_HEADER)IMAGE_FIRST_SECTION (pPEHeader);
+
+		for (int i = 0; i < pPEHeader->FileHeader.NumberOfSections; i++)
+		{
+			if ( (pSection[i].VirtualAddress == dwExportAddr) ||
+				 ((pSection[i].VirtualAddress <= dwExportAddr ) && ((pSection[i].Misc.VirtualSize+pSection[i].VirtualAddress) > dwExportAddr)) )
+			{
+				int nDiff = pSection[i].VirtualAddress-pSection[i].PointerToRawData;
+
+				PIMAGE_EXPORT_DIRECTORY pExportDir = (PIMAGE_EXPORT_DIRECTORY)&hModule[dwExportAddr-nDiff];
+
+				DWORD* pNames = (DWORD *)&hModule[pExportDir->AddressOfNames-nDiff];
+
+				for (int n = 0; n < pExportDir->NumberOfNames; n++)
+				{
+					const char *lpExportName = (const char *)&hModule[pNames[n]-nDiff];
+
+					DWORD dwCRC32 = CRC32 (0, lpExportName, strlen (lpExportName));
+
+					for (int j = 0; j < 7; j++)  // а это вам не фиг знает что, это вам оптимизаци€, типа 8-)
+						if ( dwCRC32 == ExportCRC32[j] )
+							return TRUE;
+
+/*          if ( !strcmp (lpExportName, "GetPluginInfo") ||
+             !strcmp (lpExportName, "SetStartupInfo") ||
+             !strcmp (lpExportName, "OpenPlugin") ||
+             !strcmp (lpExportName, "OpenFilePlugin") ||
+             !strcmp (lpExportName, "SetFindList") ||
+             !strcmp (lpExportName, "Configure") ||
+             !strcmp (lpExportName, "GetMinFarVersion") ||
+             !strcmp (lpExportName, "ExitFAR") )
+            return LOAD_ERROR_SUCCESS;*/
+				}
+			}
+		}
+
+		return FALSE;
+	}
+	__except (EXCEPTION_EXECUTE_HANDLER)
+	{
+		return FALSE;
+	}
+}
+
+BOOL IsModulePlugin (const wchar_t *lpModuleName)
+{
+	int bResult = FALSE;
+
+	HANDLE hModuleFile = CreateFileW (
+			lpModuleName,
+			GENERIC_READ,
+			FILE_SHARE_READ,
+			NULL,
+			OPEN_EXISTING,
+			0,
+			NULL
+			);
+
+	if ( hModuleFile != INVALID_HANDLE_VALUE )
+	{
+		HANDLE hModuleMapping = CreateFileMapping (
+				hModuleFile,
+				NULL,
+				PAGE_READONLY,
+				0,
+				0,
+				NULL
+				);
+
+		if ( hModuleMapping )
+		{
+			PBYTE pData = (PBYTE)MapViewOfFile (hModuleMapping, FILE_MAP_READ, 0, 0, 0);
+
+			bResult = IsModulePlugin2 (pData);
+
+			UnmapViewOfFile (pData);
+			CloseHandle (hModuleMapping);
+		}
+
+		CloseHandle (hModuleFile);
+	}
+
+	return bResult;
+}
+
+
 int PluginsSet::LoadPlugin(PluginItem *CurPlugin,int ModuleNumber,int Init)
 {
   if(CurPlugin->WorkFlags.Check(PIWF_DONTLOADAGAIN))
@@ -468,6 +634,10 @@ int PluginsSet::LoadPlugin(PluginItem *CurPlugin,int ModuleNumber,int Init)
       apiGetEnvironmentVariable (Drive,strCurPlugDiskPath);
     }
     PrepareModulePathW(CurPlugin->strModuleName);
+
+    if ( !IsModulePlugin (CurPlugin->strModuleName) )
+    	return FALSE;
+
     hModule=LoadLibraryExW(CurPlugin->strModuleName,NULL,LOAD_WITH_ALTERED_SEARCH_PATH);
     if(!hModule)
       LstErr=GetLastError();
@@ -478,7 +648,7 @@ int PluginsSet::LoadPlugin(PluginItem *CurPlugin,int ModuleNumber,int Init)
   }
 
   /* "...» добавь первичную загрузку с DONT_RESOLVE_DLL_REFERENCES..."  */
-  if (!hModule && !(LstErr == ERROR_BAD_EXE_FORMAT || LstErr == ERROR_PROC_NOT_FOUND))
+  if ( !hModule )
   {
     if(!Opt.LoadPlug.SilentLoadPlugin)
     {
