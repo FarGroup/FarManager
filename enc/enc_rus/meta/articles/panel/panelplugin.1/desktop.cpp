9d3748a3b5654a485c6cc6995f70f379f9e01d76
Valentin Skirdin <vskirdin@gmail.com>
2007-01-12 17:03:18 +0000

    add panel. incomplete step2 & step1

---
+++ enc/enc_rus/meta/articles/panel/panelplugin.1/desktop.cpp
@@ -0,0 +1,31 @@
+#include <windows.h>
+#include <plugin.hpp>
+#include <string.h>
+
+#include "Desktoplng.hpp"
+#include "Desktop.hpp"
+
+HANDLE WINAPI OpenPlugin(int OpenFrom,int Item)
+{
+  if(Opt.DefaultFolder[0] != 0)
+  {
+    if(OpenFrom == OPEN_PLUGINSMENU)
+    {
+      ::Info.Control(INVALID_HANDLE_VALUE,FCTL_SETPANELDIR,Opt.DefaultFolder);
+      ::Info.Control(INVALID_HANDLE_VALUE,FCTL_REDRAWPANEL,NULL);
+    }
+    else if(OpenFrom == OPEN_DISKMENU)
+    {
+      return (HANDLE)&Opt; // возвращаем все, что угодно, кроме 0 и INVALID_HANDLE_VALUE
+                           // тогда сработает функция GetFindData()
+    }
+  }
+  return INVALID_HANDLE_VALUE;
+}
+
+int WINAPI GetFindData(HANDLE hPlugin,struct PluginPanelItem **pPanelItem,
+                      int *pItemsNumber,int OpMode)
+{
+  ::Info.Control(hPlugin,FCTL_CLOSEPLUGIN,Opt.DefaultFolder);
+  return TRUE;
+}
---
+++ enc/enc_rus/meta/articles/panel/panelplugin.1/desktoplng.hpp
@@ -0,0 +1,13 @@
+enum
+{
+  MFolderPanel,
+
+  MOk,
+  MCancel,
+  MError,
+
+  MConfigTitle,
+  MConfigAddToDisksMenu,
+  MConfigDisksMenuDigit,
+  MDefaultFolder,
+};
