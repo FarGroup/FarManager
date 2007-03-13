90671f482d2b23ca19e28031b5fabf7d5e5d9a2a
Iouri Kharon <yjh@cabel.net>
2007-03-13 06:32:02 +0000

    =Adopt to A/W 'common' organization

--- plugins/helloworld/compile_vc64.bat
+++ plugins/helloworld/compile_vc64.bat
@@ -1,6 +1,6 @@
 @echo off
 call VCVARS64.BAT
-set INCLUDE=%INCLUDE%;..\common
+set INCLUDE=%INCLUDE%;..\common\ascii;..\common
 if exist HelloWorld.dll del HelloWorld.dll>nul
 cl /Wp64 /Zp8 /O1iy /GF /Gr /GR- /EHs-c- /LD /MT /Gs10000 HelloWorld.c /link /subsystem:console /machine:AMD64 /opt:nowin98 /noentry /nodefaultlib /def:HelloWorld.vc.def kernel32.lib advapi32.lib user32.lib LIBCMT.LIB /map:HelloWorld.map /merge:.rdata=.text
 if exist HelloWorld.exp del HelloWorld.exp>nul
