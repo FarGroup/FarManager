4e4bde27cc86fb2015608f4e80679c244f848780
WARP ItSelf <warpitself@gmail.com>
2008-06-05 17:58:18 +0000

    x64 extract

--- plugins/newarc/src/lib.part/Src/Rtl.Thunks.cpp
+++ plugins/newarc/src/lib.part/Src/Rtl.Thunks.cpp
@@ -10,12 +10,45 @@ byte _thunk_code[] =	{
 		};
 
 
+byte _thunk_code_x64_short[] = {
+		0x4D, 0x8B, 0xC8, //mov r9, r8
+		0x4C, 0x8B, 0xC2, //mov r8, rdx
+		0x48, 0x8B, 0xD1, //mov rdx, rcx
+		0x48, 0xB9, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //mov rcx inst
+		0x48, 0xB8, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //mov rax proc
+		0xFF, 0xE0 //jmp rax
+		};
+
+#ifdef __X64__
+
+byte* CreateThunk(void *pClass, void *pClassProc)
+{
+	byte *pThunk = (byte*)VirtualAlloc (NULL, sizeof (_thunk_code_x64_short), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	memcpy (pThunk, &_thunk_code_x64_short, sizeof (_thunk_code_x64_short));
+	memcpy (&pThunk[11], &pClass, 8);
+	memcpy (&pThunk[21], &pClassProc, 8);
+	return pThunk;
+}
+
+#else
+
 byte* CreateThunk (void *pClass, void *pClassProc)
 {
-	byte *pThunk = (byte*)VirtualAlloc (NULL, sizeof _thunk_code, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	byte *pThunk = (byte*)VirtualAlloc (NULL, sizeof (_thunk_code), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
 	memcpy (pThunk, &_thunk_code, sizeof _thunk_code);
 	memcpy (&pThunk[2], &pClass, 4);
 	memcpy (&pThunk[9], &pClassProc, 4);
+	return pThunk;
+}
+
+#endif
+
+byte* CreateThunk64_Short (void *pClass, void *pClassProc)
+{
+	byte *pThunk = (byte*)VirtualAlloc (NULL, sizeof (_thunk_code_x64_short), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	memcpy (pThunk, &_thunk_code_x64_short, sizeof (_thunk_code_x64_short));
+	memcpy (&pThunk[11], &pClass, 8);
+	memcpy (&pThunk[21], &pClassProc, 4);
 	return pThunk;
 }
 
@@ -32,6 +65,7 @@ unsigned char cdeclCode[] = {//all ecx!!
 		0x51, //push ebx
 		0xC3
 		};
+
 
 byte *CreateThunkCdecl (void *pClass, void *pClassProc)
 {
