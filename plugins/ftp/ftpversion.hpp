35b06237cb5b629a5be19b5dde98e4c3c8beeb19
Alex Yaroslavsky <trexinc@gmail.com>
2008-06-08 00:28:30 +0000

    more standardization

---
+++ plugins/ftp/ftpversion.hpp
@@ -0,0 +1,15 @@
+#ifndef __FTPVERSION_HPP__
+#define __FTPVERSION_HPP__
+#include "farversion.hpp"
+
+#define FTP_MAJOR_VER FAR_MAJOR_VER
+#define FTP_MINOR_VER FAR_MINOR_VER
+#define FTP_BUILD 242
+
+#define FTPCOMPANYNAME FARCOMPANYNAME
+#define FTPCOPYRIGHT FARCOPYRIGHT
+#define FTPPRODUCTNAME "FarFtp\000"
+#define FTPPRODUCTVERSION "1.71 build 242\000"
+#define FLLDESCRIPTION "Second-level plugin module for FAR Manager and FTP plugin\000"
+
+#endif
