c2e5aca593ff689ed12a2eeec360b67569b2cece
Valentin Skirdin <vskirdin@gmail.com>
2009-01-23 12:05:50 +0000

    add contrib & PowerShell scripts

---
+++ enc/tools/contrib/nightroman/Build-FarEnc.ps1
@@ -0,0 +1,158 @@
+
+<#
+.Synopsis
+	Builds pluginse.chm from enc_eng or pluginsr.chm from enc_rus.
+	Author: Roman Kuzmin (NightRoman)
+
+.Description
+	It processes and compiles FAR API encyclopedia source enc_eng or enc_rus
+	into pluginse.chm or pluginsr.chm
+
+	Notes
+	- Assume you have: C:\Program Files\HTML Help Workshop\hhc.exe
+	- Invoke one instance at a time, it uses the same temp files
+	- Temp directory $env:TEMP\far_enc still exists on failures
+
+.Example
+	# The current directory is enc_eng or enc_rus and CHM will be here
+	Build-FarEnc
+
+	# Build both pluginse.chm and pluginsr.chm in batch mode
+	Build-FarEnc c:\dev\enc\enc_eng c:\distr\enc -NoInteraction
+	Build-FarEnc c:\dev\enc\enc_rus c:\distr\enc -NoInteraction
+
+.Parameter InputPath
+		Directory where input items: images, meta, styles, plugins?.hhc,
+		plugins?.hhp are located. Normally it is enc_eng or enc_rus. Default:
+		the current directory.
+
+.Parameter OutputPath
+		Directory for the output pluginse.chm or pluginsr.chm. If it does not
+		exist then it is created. Default: the current directory.
+
+.Parameter NoInteraction
+		Do not invoke the result CHM.
+#>
+
+param
+(
+	[string]$InputPath = '.',
+	[string]$OutputPath = '.',
+	[switch]$NoInteraction
+)
+
+Add-Type -AssemblyName System.Web
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version 2
+
+# Ensure HTML Help Workshop
+Set-Alias hhc (Get-Command "C:\Program Files\HTML Help Workshop\hhc.exe").Definition
+
+### Some paths
+$Tmp = "$env:TEMP\far_enc"
+$InputPath = [IO.Path]::GetFullPath($InputPath)
+$OutputPath = [IO.Path]::GetFullPath($OutputPath)
+if (Test-Path "$InputPath\pluginse.hhp") {
+	$Chm = 'pluginse.chm'
+	$Hhp = 'pluginse.hhp'
+	$Hhk = 'pluginse.hhk'
+	$Out = 'distr_chm_pluginse'
+}
+elseif (Test-Path "$InputPath\pluginsr.hhp") {
+	$Chm = 'pluginsr.chm'
+	$Hhp = 'pluginsr.hhp'
+	$Hhk = 'pluginsr.hhk'
+	$Out = 'distr_chm_pluginsr'
+}
+else {
+	throw "Cannot find 'pluginse.hhp' or 'pluginsr.hhp' in '$InputPath'."
+}
+
+### Setup
+Write-Host "Starting..."
+if (!(Test-Path $OutputPath)) { $null = mkdir $OutputPath }
+if (Test-Path "$OutputPath\$Chm") { Remove-Item "$OutputPath\$Chm" }
+if (Test-Path $Tmp) { Remove-Item $Tmp -Recurse -Force }
+$null = mkdir "$Tmp\$Out"
+Write-Host "Copying data..."
+Copy-Item "$InputPath\*" $Tmp -Recurse
+Move-Item "$Tmp\meta" "$Tmp\html"
+Push-Location $Tmp
+
+### Index
+Write-Host "Making $Hhk..."
+$nbIndex = 0
+Set-Content $Hhk $(
+	@'
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<HTML>
+<BODY>
+<UL>
+'@
+	# index <h1> and <a name=..>
+	$map = New-Object Collections.Hashtable
+	foreach($file in Get-ChildItem html -Include *.html -Recurse -Name) {
+		$h1 = $null
+		$path = "html\$file"
+		foreach($line in Get-Content $path) {
+			$name = $null
+			$href = $null
+			$page = $null
+			# index all <h1>, assume HTML tags are not used inside
+			if ($line -match '<h1>(.+?)</h1>') {
+				$h1 = [System.Web.HttpUtility]::HtmlDecode($matches[1]).Replace('"', '')
+				$name = $h1
+				$href = $path
+			}
+			# index <a> without HTML tags inside
+			elseif ($line -match '<a\s+.*?name\s*=\s*["''](.+?)["''].*?>([^<>]+)</a>') {
+				$name = [System.Web.HttpUtility]::HtmlDecode($matches[2]).Replace('"', '')
+				$href = $path + '#' + $matches[1]
+				$page = $h1
+			}
+			if ($name) {
+				++$nbIndex
+				$item = 1 | Select-Object href, page
+				$item.href = $href
+				$item.page = $page
+				if ($map.ContainsKey($name)) {
+					$map[$name] += $item
+				}
+				else {
+					$map.Add($name, @($item))
+				}
+			}
+		}
+	}
+	foreach($name in $map.Keys | Sort-Object) {
+		$items = $map[$name]
+		foreach($item in $items | Sort-Object page) {
+			@"
+<LI><OBJECT type="text/sitemap">
+<param name="Name" value="$(if ($item.page -and $items.Count -ge 2) { $name + ' - ' + $item.page } else { $name })">
+<param name="Local" value="$($item.href)">
+</OBJECT>
+"@
+		}
+	}
+	@'
+</UL>
+</BODY>
+</HTML>
+'@
+)
+
+### CHM
+Write-Host "Building CHM..."
+hhc "$Tmp\$Hhp"
+Copy-Item "$Out\$Chm" $OutputPath
+if (!$NoInteraction) { Invoke-Item "$OutputPath\$Chm" }
+
+### OK
+Write-Host "Finishing..."
+Pop-Location
+Remove-Item $Tmp -Recurse -Force
+Write-Host @"
+Indices: $nbIndex
+OK
+"@
---
+++ enc/tools/contrib/nightroman/Test-HtmlLink.ps1
@@ -0,0 +1,60 @@
+
+<#
+.Synopsis
+	Tests href and src links in HTML pages.
+	Author: Roman Kuzmin (NightRoman)
+
+.Description
+	It scans local href and src links in HTML and checks that targets exist.
+	For a missed target the script writes text info or an object with
+	properties: Path, LineNumber, Link.
+
+.Example
+	# Test links in two directories, show text info
+	Test-HtmlLink enc_eng\meta, enc_rus\meta
+
+	# Ditto, but emit objects and format them as list
+	Test-HtmlLink enc_eng\meta, enc_rus\meta -Emit | Format-List
+
+	# Emitted objects can be used effectively, for example in PowerShellFar we
+	# can get bad links, show list of them and for a selected one open the
+	# source HTML in editor right at the line with a link
+	Test-HtmlLink -Emit | Out-FarList -Text Link | Start-FarEditor
+
+.Parameter Path
+		Location(s) of HTML pages. See Get-ChildItem -Path. Default is the
+		current location.
+
+.Parameter Include
+		See Get-ChildItem -Include. Default: *.htm and *.html.
+
+.Parameter Emit
+		Tells to emit objects.
+#>
+
+param
+(
+	$Path = '.',
+	$Include = @('*.htm', '*.html'),
+	[switch]$Emit
+)
+
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version 2
+
+Get-ChildItem $Path -Include $Include -Recurse | Select-String '(?:href|src)\s*=\s*["'']([^"''#]+?)["''#]' -AllMatches | .{process{
+	$directory = [IO.Path]::GetDirectoryName($_.Path)
+	foreach($m in $_.Matches) {
+		$link = $m.Groups[1].Value
+		if ($link -notmatch '^(?:win32/|http:|mailto:|ftp:|news:)' -and ![IO.File]::Exists("$directory\$link")) {
+			if ($Emit) {
+				$e = 1 | Select-Object Path, LineNumber, Link
+				$e.Path = $_.Path; $e.LineNumber = $_.LineNumber; $e.Link = $link
+				$e
+			}
+			else {
+				'{0}:{1}:{2}' -f $_.Path, $_.LineNumber, $link
+			}
+		}
+	}
+}}
