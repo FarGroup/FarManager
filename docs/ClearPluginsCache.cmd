e7da5f2b0c2b72f1bd09e804defd56043c8b07d1
Alex Alabuzhev <alabuzhev@gmail.com>
2009-05-30 13:14:35 +0000

    cosmetic

--- docs/ClearPluginsCache.cmd
+++ docs/ClearPluginsCache.cmd
@@ -2,4 +2,4 @@
 :: This batch file cleares the plugins cache
 
 reg query hkcu\software\far2\pluginscache /ve > nul 2>^&1
-IF %errorlevel% equ 0 reg delete hkcu\software\far2\pluginscache /f
+if %errorlevel% equ 0 reg delete hkcu\software\far2\pluginscache /f
--- docs/SaveSettings.cmd
+++ docs/SaveSettings.cmd
@@ -3,6 +3,6 @@
 :: to files FarSave1.reg and FarSave2.reg
 
 reg query hkcu\software\far2 /ve > nul 2>^&1
-IF %errorlevel% equ 0 reg export hkcu\software\far2 %~dp0FarSave1.reg /y
+if %errorlevel% equ 0 reg export hkcu\software\far2 %~dp0FarSave1.reg /y
 reg query hklm\software\far2 /ve > nul 2>^&1
-IF %errorlevel% equ 0 reg export hklm\software\far2 %~dp0FarSave2.reg /y
+if %errorlevel% equ 0 reg export hklm\software\far2 %~dp0FarSave2.reg /y
