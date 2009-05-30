93204ec9cf305740549b86ec4bff92547041f4f0
Alex Yaroslavsky <trexinc@gmail.com>
2009-05-30 13:08:30 +0000

    grammar

--- docs/ClearPluginsCache.cmd
+++ docs/ClearPluginsCache.cmd
@@ -1,6 +1,5 @@
 @echo off
-:: This batch file cleared the plugins cache
+:: This batch file cleares the plugins cache
 
-reg query hkcu\software\far2\pluginscache /ve > nul 2>^&1 
+reg query hkcu\software\far2\pluginscache /ve > nul 2>^&1
 IF %errorlevel% equ 0 reg delete hkcu\software\far2\pluginscache /f
-
