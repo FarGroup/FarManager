b0fc9f7f1152a27eebba058a15cd98d39691ad69
Alex Yaroslavsky <trexinc@gmail.com>
2009-11-28 11:59:58 +0000

    use vc8 for installer

--- misc/nightly/installer.64.bat
+++ misc/nightly/installer.64.bat
@@ -1,11 +1,33 @@
-call c:\VC9\vcvarsall.bat x86
+SET MSSdk=C:\VC8
+Set SdkTools=%MSSdk%\Bin
+Set OSLibraries=%MSSdk%\Lib
+Set OSIncludes=%MSSdk%\Include;%MSSdk%\Include\gl
+Set VCTools=%MSSdk%\Bin
+Set VCLibraries=%MSSdk%\Lib
+Set VCIncludes=%MSSdk%\Include;%MSSdk%\Include\Sys
+Set ReferenceAssemblies=%ProgramFiles%\Reference Assemblies\Microsoft\WinFX\v3.0
+:: Setting the path
+SET Path=%VCTools%;%SdkTools%;%Path%
+SET Lib=%VCLibraries%;%OSLibraries%;%Lib%
+SET Include=%VCIncludes%;%OSIncludes%;%Include%
 SET CPU=i386
 SET APPVER=6.0
 SET Path=%Path%;C:\Program Files\Windows Installer XML v3\bin
 
 cl -EHsc -DSPECIAL genscript.cpp
 
-call c:\VC9\vcvarsall.bat x86_amd64
+SET MSSdk=C:\VC8
+Set SdkTools=%MSSdk%\Bin
+Set OSLibraries=%MSSdk%\Lib\x64
+Set OSIncludes=%MSSdk%\Include;%MSSdk%\Include\gl
+Set VCTools=%MSSdk%\Bin\x86_x64
+Set VCLibraries=%MSSdk%\Lib\x64
+Set VCIncludes=%MSSdk%\Include;%MSSdk%\Include\Sys
+Set ReferenceAssemblies=%ProgramFiles%\Reference Assemblies\Microsoft\WinFX\v3.0
+:: Setting the path
+SET Path=%VCTools%;%SdkTools%;%Path%
+SET Lib=%VCLibraries%;%OSLibraries%;%Lib%
+SET Include=%VCIncludes%;%OSIncludes%;%Include%
 SET CPU=AMD64
 SET APPVER=6.0
 
