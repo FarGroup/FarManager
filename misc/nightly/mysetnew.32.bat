566705033208ad884730697d5235bab44ac0b511
Alex Yaroslavsky <trexinc@gmail.com>
2009-11-29 00:28:10 +0000

    NO_RELEASE_PDB=1

--- misc/nightly/mysetnew.32.bat
+++ misc/nightly/mysetnew.32.bat
@@ -2,4 +2,4 @@ call c:\VC9\vcvarsall.bat x86
 SET CPU=i386
 SET APPVER=6.0
 
-nmake /f makefile_vc build USEDEPS=1
+nmake /f makefile_vc build USEDEPS=1 NO_RELEASE_PDB=1
--- misc/nightly/mysetnew.64.bat
+++ misc/nightly/mysetnew.64.bat
@@ -2,4 +2,4 @@ call c:\VC9\vcvarsall.bat x86_amd64
 SET CPU=AMD64
 SET APPVER=6.0
 
-nmake /f makefile_vc build USEDEPS=1
+nmake /f makefile_vc build USEDEPS=1 NO_RELEASE_PDB=1
