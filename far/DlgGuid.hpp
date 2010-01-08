6138be5707a60e97fcc5acf0bf72527fcfc7f628
Vadim Yegorov <oo.zg.oo@gmail.com>
2010-01-08 11:47:34 +0000

    lost files.

---
+++ far/DlgGuid.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+/*
+DlgGuid.hpp
+
+GUID'ы диалогов.
+*/
+/*
+Copyright (c) 2010 Far Group
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the authors may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef __DLGGUID_HPP__
+#define __DLGGUID_HPP__
+
+// {8C9EAD29-910F-4b24-A669-EDAFBA6ED964}
+DEFINE_GUID(FindFileId,0x8c9ead29,0x910f,0x4b24,0xa6,0x69,0xed,0xaf,0xba,0x6e,0xd9,0x64);
+// {9FBCB7E1-ACA2-475d-B40D-0F7365B632FF}
+DEFINE_GUID(CopyOverwriteId,0x9fbcb7e1,0xaca2,0x475d,0xb4,0xd,0xf,0x73,0x65,0xb6,0x32,0xff);
+
+#endif
