fb38208607cef75fc6f7f11753e45c0fc9583b07
WARP ItSelf <warpitself@gmail.com>
2010-02-13 09:35:43 +0000

    lng part1

--- plugins/newarc.ex/Newarc/Locale/build.bat
+++ plugins/newarc.ex/Newarc/Locale/build.bat
@@ -1,4 +1,7 @@
-lng.exe newarc.feed
+echo "Building Lanuage Files"
+echo %1
+
+"..\..\Tools\lng.generator.exe" newarc.feed
 move "newarc.Messages.h" "../Include/newarc.Messages.h"
-move "newarc.Russian.lng" "C:\System Independent Files\Manager\Far\Plugins\newarc\newarc.Russian.lng"
-move "newarc.English.lng" "C:\System Independent Files\Manager\Far\Plugins\newarc\newarc.English.lng"
+move "newarc.Russian.lng" "../../Bin/%1/newarc.Russian.lng"
+move "newarc.English.lng" "../../Bin/%1/newarc.English.lng"
