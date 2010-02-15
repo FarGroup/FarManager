905ff80ea201d1556676ba9fca8c3e9414d1efe8
Alex Yaroslavsky <trexinc@gmail.com>
2010-02-15 16:08:05 +0000

    check if tag exists before tagging

--- far/svn_tag_build.bat
+++ far/svn_tag_build.bat
@@ -1,18 +1,17 @@
 @echo --------------------------------------------------------------------
 @echo Continue only if you are sure that you have set the correct
 @echo build and commited the changes.
-@echo This command will tag the trunk under tags/VERSION_bBUILD, if such
-@echo tag already exists this command might cause problems.
+@echo This command will tag the trunk under tags/VERSION_bBUILD.
 @echo --------------------------------------------------------------------
 @echo If you're not sure press CtrlC.
 @echo --------------------------------------------------------------------
 @echo --------------------------------------------------------------------
 @echo Продолжайте тольке если вы уверены что вы выставили правильный
 @echo номер билда и закомитили изменения.
-@echo Эта команда пометит текущий trunk в tags/VERSION_bBUILD, если такая
-@echo пометка уже существует, то это может привести к проблемам.
+@echo Эта команда пометит текущий trunk в tags/VERSION_bBUILD.
 @echo --------------------------------------------------------------------
 @echo Если вы не уверены, то нажмите CtrlC
 @echo --------------------------------------------------------------------
 @pause
+@echo.
 @tools\m4 -P svn_tag_build.m4
