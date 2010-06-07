17c28ce67479fa36c06f192f5053b0e3bd92e6d8
Iouri Kharon <yjh@cabel.net>
2010-06-07 11:49:55 +0000

    Added oem(0xFF) (soft-space) to QoutedSymbols

--- far/config.cpp
+++ far/config.cpp
@@ -664,7 +664,7 @@ static struct FARConfig
 	{0, REG_DWORD,  NKeySystem,L"SubstNameRule", &Opt.SubstNameRule, 2, 0},
 	{0, REG_DWORD,  NKeySystem,L"ShowCheckingFile", &Opt.ShowCheckingFile, 0, 0},
 	{0, REG_DWORD,  NKeySystem,L"DelThreadPriority", &Opt.DelThreadPriority, THREAD_PRIORITY_NORMAL, 0},
-	{0, REG_SZ,     NKeySystem,L"QuotedSymbols",&Opt.strQuotedSymbols, 0, L" &()[]{}^=;!'+,`"},
+  {0, REG_SZ,     NKeySystem,L"QuotedSymbols",&Opt.strQuotedSymbols, 0, L" &()[]{}^=;!'+,`\xA0"}, //xA0 => 160 =>oem(0xFF)
 	{0, REG_DWORD,  NKeySystem,L"QuotedName",&Opt.QuotedName,0xFFFFFFFFU, 0},
 	//{0, REG_DWORD,  NKeySystem,L"CPAJHefuayor",&Opt.strCPAJHefuayor,0, 0},
 	{0, REG_DWORD,  NKeySystem,L"CloseConsoleRule",&Opt.CloseConsoleRule,1, 0},
--- far/vbuild.m4
+++ far/vbuild.m4
@@ -1,1 +1,1 @@
-m4_define(BUILD,1579)m4_dnl
+m4_define(BUILD,1580)m4_dnl
