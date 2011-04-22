fc895ca0bc22eccf003e3d852cc1b4d6ab4f57d4
WARP ItSelf <warpitself@gmail.com>
2011-04-22 02:52:15 +0000

    oops

--- plugins/newarc.ex/Modules/7z/Source/7z.cpp
+++ plugins/newarc.ex/Modules/7z/Source/7z.cpp
@@ -13,8 +13,6 @@ int OnInitialize (StartupInfo *pInfo)
 	pModule = new SevenZipModule;
 	pModule->Load();
 
-	MessageBoxW(0, Info.GetMsg(Info.ModuleNumber, 0), _T("123"), MB_OK);
-
 	return NAERROR_SUCCESS;
 }
 
