24bf853a0b27fb35c8da15dfe8a530f6d40df7e8
Oleg Makovski <farplug@gmail.com>
2011-05-26 07:56:46 +0000

    arclite: Far 3 API

--- plugins/arclite/plugin_h.h
+++ plugins/arclite/plugin_h.h
@@ -1,6 +1,6 @@
 #include "farversion.hpp"
 #define PLUGIN_NAME L"<(NAME)>"
-#define PLUGIN_VERSION MAKEFARVERSION(<(VER_MAJOR)>, <(VER_MINOR)>, <(VER_PATCH)>, 0)
+#define PLUGIN_VERSION MAKEFARVERSION(<(VER_MAJOR)>, <(VER_MINOR)>, <(VER_PATCH)>, 0, VS_RELEASE)
 #ifdef x64
 #define PLUGIN_DESCRIPTION L"<(NAME)> for Far Manager <(FAR_VER_MAJOR)> x64"
 #else
