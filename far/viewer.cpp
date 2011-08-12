d3519fadd62cfbfbd57cf731768de4cef9111789
Valentin Skirdin <vskirdin@gmail.com>
2011-08-12 05:35:21 +0000

    Mantis#0001766

--- far/viewer.cpp
+++ far/viewer.cpp
@@ -1266,6 +1266,8 @@ int Viewer::ProcessKey(int Key)
 				if (!apiGetFindDataEx(strFullFileName, NewViewFindData))
 					return TRUE;
 
+				ViewFile.GetSize(NewViewFindData.nFileSize); // Required! -- thanks Dzirt2005
+
 				if (ViewFindData.ftLastWriteTime.dwLowDateTime!=NewViewFindData.ftLastWriteTime.dwLowDateTime
 				 || ViewFindData.ftLastWriteTime.dwHighDateTime!=NewViewFindData.ftLastWriteTime.dwHighDateTime
 				 || ViewFindData.nFileSize != NewViewFindData.nFileSize)
