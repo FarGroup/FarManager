f4177da4082b1a3f91ac2506d150ba2461012564
Vadim Yegorov <oo.zg.oo@gmail.com>
2011-09-03 10:09:25 +0000

    update headers to 2189

--- plugins/common/unicode/farcolor.hpp
+++ plugins/common/unicode/farcolor.hpp
@@ -4,7 +4,7 @@
 /*
   farcolor.hpp
 
-  Colors Index for FAR Manager 3.0 build 2184
+  Colors Index for FAR Manager 3.0 build 2188
 */
 
 /*
--- plugins/common/unicode/plugin.hpp
+++ plugins/common/unicode/plugin.hpp
@@ -5,7 +5,7 @@
 /*
   plugin.hpp
 
-  Plugin API for Far Manager 3.0 build 2184
+  Plugin API for Far Manager 3.0 build 2188
 */
 
 /*
@@ -43,7 +43,7 @@ other possible license with no implications from the above license on them.
 #define FARMANAGERVERSION_MAJOR 3
 #define FARMANAGERVERSION_MINOR 0
 #define FARMANAGERVERSION_REVISION 0
-#define FARMANAGERVERSION_BUILD 2184
+#define FARMANAGERVERSION_BUILD 2188
 #define FARMANAGERVERSION_STAGE VS_RELEASE
 
 #ifndef RC_INVOKED
@@ -612,7 +612,7 @@ struct FarMenuItem
 {
 	MENUITEMFLAGS Flags;
 	const wchar_t *Text;
-	DWORD AccelKey;
+	struct FarKey AccelKey;
 	DWORD_PTR Reserved;
 	DWORD_PTR UserData;
 };
