91f2ec7ca3b067c3af888b61330cb27a0df179c2
Vladimir Surguchev <vladimir.surguchev@gmail.com>
2011-11-04 16:09:51 +0000

    typo fixed in api_test.c

--- far/api_test.c
+++ far/api_test.c
@@ -1,7 +1,7 @@
 #include "Include/plugin.hpp"
 #include "Include/farcolor.hpp"
 #ifdef __cplusplus
-#if (defined(__GNUC__) && (100*__GNUC__+10*GNUC_MINOR__+__GNUC_PATCHLEVEL__) < 461) || (defined(_MSC_VER) && _MSC_VER<1600)
+#if (defined(__GNUC__) && (100*__GNUC__+10*__GNUC_MINOR__+__GNUC_PATCHLEVEL__) < 461) || (defined(_MSC_VER) && _MSC_VER<1600)
 #define nullptr NULL
 #endif
 #include "Include/DlgBuilder.hpp"
