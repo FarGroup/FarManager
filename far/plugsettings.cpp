d7536e2f3d4a42d00cec1c24129678e64701e294
Alex Alabuzhev <alabuzhev@gmail.com>
2011-11-30 19:48:49 +0000

    add descriptions to plugins db

--- far/plugsettings.cpp
+++ far/plugsettings.cpp
@@ -40,6 +40,8 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "datetime.hpp"
 #include "FarGuid.hpp"
 #include "shortcuts.hpp"
+#include "dizlist.hpp"
+#include "config.hpp"
 
 template<> void DeleteItems<FarSettingsHistory>(FarSettingsHistory* Items,size_t Size)
 {
@@ -71,6 +73,18 @@ PluginSettings::PluginSettings(const GUID& Guid) : PluginsCfg(nullptr)
 		PluginsCfg = CreatePluginsConfig(strGuid);
 		unsigned __int64& root(*m_Keys.insertItem(0));
 		root=PluginsCfg->CreateKey(0, strGuid, pPlugin->GetTitle());
+
+		DizList Diz;
+		string strDbPath = Opt.ProfilePath + L"\\PluginsData\\";
+		Diz.Read(strDbPath);
+		string strDbName = strGuid + L".db";
+		string Description =  string(pPlugin->GetTitle()) + L" (" + pPlugin->GetDescription() + L")";
+		if(StrCmp(Diz.GetDizTextAddr(strDbName, L"", 0), Description))
+		{
+			Diz.AddDizText(strDbName, L"", Description);
+			Diz.Flush(strDbPath);
+		}
+
 	}
 }
 
--- far/vbuild.m4
+++ far/vbuild.m4
@@ -1,1 +1,1 @@
-m4_define(BUILD,2272)m4_dnl
+m4_define(BUILD,2273)m4_dnl
