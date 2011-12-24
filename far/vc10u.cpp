718fc5612349c29c6feea97aa16f551c06b6d414
Alex Yaroslavsky <trexinc@gmail.com>
2011-12-24 11:17:04 +0000

    restore, deleted by mistake

---
+++ far/vc10u.cpp
@@ -0,0 +1,63 @@
+/*
+vc10u.cpp
+
+Workaround for VC2010 and old Windows
+*/
+/*
+Copyright © 2010 Far Group
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the authors may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <headers.hpp>
+#pragma hdrstop
+
+#include <delayimp.h>
+
+//----------------------------------------------------------------------------
+static LPVOID WINAPI no_recode_pointer(LPVOID p)
+{
+    return p;
+}
+
+//----------------------------------------------------------------------------
+static FARPROC WINAPI delayFailureHook(/*dliNotification*/unsigned dliNotify,
+                                       PDelayLoadInfo pdli)
+{
+    if(   dliNotify == /*dliFailGetProcAddress*/dliFailGetProc
+       && pdli && pdli->cb == sizeof(*pdli)
+       && pdli->hmodCur == GetModuleHandleA("kernel32")
+       && pdli->dlp.fImportByName && pdli->dlp.szProcName
+       && (   !lstrcmpA(pdli->dlp.szProcName, "EncodePointer")
+           || !lstrcmpA(pdli->dlp.szProcName, "DecodePointer")))
+    {
+      return (FARPROC)no_recode_pointer;
+    }
+    return nullptr;
+}
+
+//----------------------------------------------------------------------------
+PfnDliHook __pfnDliFailureHook2 = (PfnDliHook)delayFailureHook;
+
+//----------------------------------------------------------------------------
