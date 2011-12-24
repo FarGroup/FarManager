920a821fcfbda445e53f65ddc8ff1a7d6c5592d9
Alex Yaroslavsky <trexinc@gmail.com>
2011-12-24 12:47:26 +0000

    update to using WIX 3.5

--- misc/nightly/installer.32.bat
+++ misc/nightly/installer.32.bat
@@ -1,7 +1,7 @@
 call c:\VC10\vcvarsall.bat x86
 SET CPU=i386
 SET APPVER=6.0
-SET Path=%Path%;C:\Program Files\Windows Installer XML v3\bin
+SET Path=%Path%;C:\Program Files\Windows Installer XML v3.5\bin
 
 cl -nologo -EHsc -DNIGHTLY genparams.cpp
 
