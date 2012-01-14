ec6293815f9e6b36c3692e80012032e2c5864412
Alex Yaroslavsky <trexinc@gmail.com>
2012-01-14 09:40:23 +0000

    update headers to 2378

--- plugins/common/unicode/plugin.hpp
+++ plugins/common/unicode/plugin.hpp
@@ -5,7 +5,7 @@
 /*
   plugin.hpp
 
-  Plugin API for Far Manager 3.0 build 2377
+  Plugin API for Far Manager 3.0 build 2378
 */
 
 /*
@@ -43,7 +43,7 @@ other possible license with no implications from the above license on them.
 #define FARMANAGERVERSION_MAJOR 3
 #define FARMANAGERVERSION_MINOR 0
 #define FARMANAGERVERSION_REVISION 0
-#define FARMANAGERVERSION_BUILD 2377
+#define FARMANAGERVERSION_BUILD 2378
 #define FARMANAGERVERSION_STAGE VS_RELEASE
 
 #ifndef RC_INVOKED
