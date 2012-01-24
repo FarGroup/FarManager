ee1c4ccb1f760734088d41eb73d11a1ca4877dd2
Oleg Makovski <farplug@gmail.com>
2012-01-24 13:33:31 +0000

    arclite: more clear plugin description

--- plugins/arclite/plugin_h.h
+++ plugins/arclite/plugin_h.h
@@ -1,8 +1,4 @@
 #include "farversion.hpp"
 #define PLUGIN_NAME L"<(NAME)>"
 #define PLUGIN_VERSION MAKEFARVERSION(<(VER_MAJOR)>, <(VER_MINOR)>, <(VER_PATCH)>, 0, VS_RELEASE)
-#ifdef x64
-#define PLUGIN_DESCRIPTION L"<(NAME)> for Far Manager <(FAR_VER_MAJOR)> x64"
-#else
-#define PLUGIN_DESCRIPTION L"<(NAME)> for Far Manager <(FAR_VER_MAJOR)>"
-#endif
+#define PLUGIN_DESCRIPTION L"Archive support (based on 7-Zip project)"
