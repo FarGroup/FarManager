70e07930b523b4e2df30aca4bfefda5e24c7029e
Vladimir Surguchev <vladimir.surguchev@gmail.com>
2012-03-11 20:05:02 +0000

    M#2049

--- far/edit.cpp
+++ far/edit.cpp
@@ -3179,8 +3179,14 @@ void EnumFiles(VMenu& Menu, const wchar_t* Str)
 				if(NameMatch || AltNameMatch)
 				{
 					strStr.SetLength(FileName-strStr);
-					string strTmp(strStart+strStr);
-					strTmp+=NameMatch?d.strFileName:d.strAlternateFileName;
+					string strAdd (strStr + (NameMatch ? d.strFileName : d.strAlternateFileName));
+					if (!StartQuote)
+						QuoteSpace(strAdd);
+
+					string strTmp(strStart+strAdd);
+					if(StartQuote)
+						strTmp += L'"';
+
 					if(!Separator)
 					{
 						if(Menu.GetItemCount())
@@ -3195,10 +3201,6 @@ void EnumFiles(VMenu& Menu, const wchar_t* Str)
 							Menu.SetTitle(MSG(MCompletionFilesTitle));
 						}
 						Separator=true;
-					}
-					if(StartQuote)
-					{
-						strTmp+=L'"';
 					}
 					Menu.AddItem(strTmp);
 				}
--- far/vbuild.m4
+++ far/vbuild.m4
@@ -1,1 +1,1 @@
-m4_define(BUILD,2536)m4_dnl
+m4_define(BUILD,2537)m4_dnl
