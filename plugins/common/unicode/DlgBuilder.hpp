57641fc1b4c3d5706c247b0f795e35feb8bbbffb
Vadim Yegorov <oo.zg.oo@gmail.com>
2012-09-21 23:33:12 +0000

    update headers to 2822

--- plugins/common/unicode/DlgBuilder.hpp
+++ plugins/common/unicode/DlgBuilder.hpp
@@ -9,7 +9,7 @@
 /*
   DlgBuilder.hpp
 
-  Dynamic construction of dialogs for FAR Manager 3.0 build 2821
+  Dynamic construction of dialogs for FAR Manager 3.0 build 2822
 */
 
 /*
--- plugins/common/unicode/plugin.hpp
+++ plugins/common/unicode/plugin.hpp
@@ -5,7 +5,7 @@
 /*
   plugin.hpp
 
-  Plugin API for Far Manager 3.0 build 2821
+  Plugin API for Far Manager 3.0 build 2822
 */
 
 /*
@@ -43,7 +43,7 @@ other possible license with no implications from the above license on them.
 #define FARMANAGERVERSION_MAJOR 3
 #define FARMANAGERVERSION_MINOR 0
 #define FARMANAGERVERSION_REVISION 0
-#define FARMANAGERVERSION_BUILD 2821
+#define FARMANAGERVERSION_BUILD 2822
 #define FARMANAGERVERSION_STAGE VS_RELEASE
 
 #ifndef RC_INVOKED
@@ -2197,8 +2197,8 @@ struct FarGetPluginInformation
 	size_t StructSize;
 	const wchar_t *ModuleName;
 	FAR_PLUGIN_FLAGS Flags;
-	struct PluginInfo PInfo;
-	struct GlobalInfo GInfo;
+	struct PluginInfo *PInfo;
+	struct GlobalInfo *GInfo;
 };
 
 struct InfoPanelLine
