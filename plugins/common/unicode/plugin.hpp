2bf725bdc2d14f054d7fa35a56ab4619dfcf32a9
Vadim Yegorov <oo.zg.oo@gmail.com>
2012-09-23 18:00:28 +0000

    update headers to 2834

--- plugins/common/unicode/plugin.hpp
+++ plugins/common/unicode/plugin.hpp
@@ -5,7 +5,7 @@
 /*
   plugin.hpp
 
-  Plugin API for Far Manager 3.0 build 2832
+  Plugin API for Far Manager 3.0 build 2834
 */
 
 /*
@@ -43,7 +43,7 @@ other possible license with no implications from the above license on them.
 #define FARMANAGERVERSION_MAJOR 3
 #define FARMANAGERVERSION_MINOR 0
 #define FARMANAGERVERSION_REVISION 0
-#define FARMANAGERVERSION_BUILD 2832
+#define FARMANAGERVERSION_BUILD 2834
 #define FARMANAGERVERSION_STAGE VS_RELEASE
 
 #ifndef RC_INVOKED
@@ -689,8 +689,8 @@ struct PluginPanelItem
 	PLUGINPANELITEMFLAGS Flags;
 	struct
 	{
-		void* UserData;
-		FARPANELITEMFREECALLBACK Callback;
+		void* Data;
+		FARPANELITEMFREECALLBACK FreeData;
 	} UserData;
 	uintptr_t FileAttributes;
 	uintptr_t NumberOfLinks;
