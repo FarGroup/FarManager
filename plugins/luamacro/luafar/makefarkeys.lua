fb604a838bde9b7559f88fb2872c565c0757f50b
Alex Alabuzhev <alabuzhev@gmail.com>
2012-09-29 21:52:04 +0000

    fix build

--- plugins/luamacro/luafar/makefarkeys.lua
+++ plugins/luamacro/luafar/makefarkeys.lua
@@ -14,13 +14,10 @@ local function extract_enums (src)
   return collector
 end
 
+ 
 local sTmpFile = [[
-#include <windows.h>
-#include <stdio.h>
-#include <farcolor.hpp>
-
-int main() {
 ]]
+
 
 local sOutFile = [[
 // This is a generated file.
@@ -39,31 +36,13 @@ void SetFarColors (lua_State *L)
 ]]
 
 local function get_insert (in_dir, src)
-  local tb = { sTmpFile, }
+  local tb = {}
+  local i = 0
   for _,v in ipairs(extract_enums(src)) do
-    if v:match("^%s*#") then table.insert(tb, v)
-    else table.insert(tb, ('  printf("%s=%%u,", (unsigned int)%s);'):format(v,v))
-    end
+    table.insert(tb, ('%s=%d,'):format(v,i))
+    i = i + 1
   end
-  table.insert(tb, "  return 0;\n}\n")
-
-  local fp = assert(io.open("tmp.c", "w"))
-  fp:write(table.concat(tb, "\n"))
-  fp:close()
-
-  -- Note 1: "-m32" makes possible to cross-compile LuaFAR for 64-bit target on 32-bit OS.
-  -- Note 2: "-DWINVER=0x601" corresponds to Windows 7.
-  -- assert(0 == os.execute("gcc -o tmp.exe -m32 -DWINVER=0x601 -I"..in_dir.." tmp.c"))
-
-  assert((0 == os.execute("cl.exe /nologo /I"..in_dir.." tmp.c > nul 2>&1")) or (0 == os.execute("gcc -o tmp.exe -m32 -DWINVER=0x601 -I"..in_dir.." tmp.c > nul 2>&1")))
-
-  fp = assert(io.popen("tmp.exe"))
-  local str = fp:read("*all")
-  fp:close()
-  os.remove "tmp.exe"
-  os.remove "tmp.c"
-  os.remove "tmp.obj"
-  return str
+  return table.concat(tb, " ")
 end
 
 local function makefarcolors (in_dir, out_file)
