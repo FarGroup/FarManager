013807c32fae8a64ca6d69de1bc340ee5cf85d37
Alex Yaroslavsky <trexinc@gmail.com>
2012-10-06 19:01:25 +0000

    x64 warnings

--- plugins/tmppanel/version.hpp
+++ plugins/tmppanel/version.hpp
@@ -1,6 +1,6 @@
 #include "farversion.hpp"
 
-#define PLUGIN_BUILD 100
+#define PLUGIN_BUILD 101
 #define PLUGIN_DESC L"Temporary Panel for Far Manager"
 #define PLUGIN_NAME L"TmpPanel"
 #define PLUGIN_FILENAME L"TmpPanel.dll"
