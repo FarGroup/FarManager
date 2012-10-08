4f402a53cfbc711c06ce4d1d64474cc0f106fb0d
Alex Yaroslavsky <trexinc@gmail.com>
2012-10-08 19:29:41 +0000

    update headers to 2878

--- plugins/common/unicode/plugin.hpp
+++ plugins/common/unicode/plugin.hpp
@@ -5,7 +5,7 @@
 /*
   plugin.hpp
 
-  Plugin API for Far Manager 3.0 build 2872
+  Plugin API for Far Manager 3.0 build 2878
 */
 
 /*
@@ -43,7 +43,7 @@ other possible license with no implications from the above license on them.
 #define FARMANAGERVERSION_MAJOR 3
 #define FARMANAGERVERSION_MINOR 0
 #define FARMANAGERVERSION_REVISION 0
-#define FARMANAGERVERSION_BUILD 2872
+#define FARMANAGERVERSION_BUILD 2878
 #define FARMANAGERVERSION_STAGE VS_RELEASE
 
 #ifndef RC_INVOKED
@@ -1511,8 +1511,8 @@ struct EditorInfo
 	intptr_t BlockStartLine;
 	uintptr_t Options;
 	intptr_t TabSize;
-	intptr_t BookmarkCount;
-	intptr_t SessionBookmarkCount;
+	size_t BookmarkCount;
+	size_t SessionBookmarkCount;
 	uintptr_t CurState;
 	uintptr_t CodePage;
 };
