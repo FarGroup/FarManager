e934e4c010e47e6c36d5acc109702abc4bf7146a
Alex Yaroslavsky <trexinc@gmail.com>
2012-11-07 22:31:53 +0000

    new post commit hook

---
+++ enc/tools/tool.make_web_post_commit.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# For use as a post commit hook in svn (environment not set)
+# $1 = path to repository
+# $2 = number of revision commited
+#
+
+/usr/bin/svnlook changed "$1" -r "$2" | /bin/grep trunk/enc
+if [ $? -ne 0 ]; then exit 0; fi
+
+/usr/bin/svn co -q --force http://localhost/svn/trunk/enc/ /var/tmp/enc
+
+# need to set up path as the script will try to run svn
+export PATH=$PATH:/usr/bin
+cd /var/tmp/enc/tools && /usr/bin/python ./tool.make_inet.py
+if [ $? -ne 0 ]; then exit 0; fi
+cd /
+
+/bin/rm -Rf /var/www/api/*
+/bin/cp -Rf /var/tmp/enc/build/inet/* /var/www/api/
+/bin/rm -Rf /var/tmp/enc
