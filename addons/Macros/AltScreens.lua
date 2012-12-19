3503412573e7ca3a9ef93ea8aebc420fa7d4e06a
Alex Alabuzhev <alabuzhev@gmail.com>
2012-12-19 19:22:09 +0000

    removed unused flags

--- addons/Macros/AltScreens.lua
+++ addons/Macros/AltScreens.lua
@@ -1,179 +1,179 @@
 Macro {
-  area="Shell"; key="Alt1"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-1 to switch between screens"; action = function()
+  area="Shell"; key="Alt1"; flags="DisableOutput"; description="Use Alt-1 to switch between screens"; action = function()
 Keys('F12 1')
   end;
 }
 
 Macro {
-  area="Shell"; key="Alt2"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-2 to switch between screens"; action = function()
+  area="Shell"; key="Alt2"; flags="DisableOutput"; description="Use Alt-2 to switch between screens"; action = function()
 Keys('F12 2')
   end;
 }
 
 Macro {
-  area="Shell"; key="Alt3"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-3 to switch between screens"; action = function()
+  area="Shell"; key="Alt3"; flags="DisableOutput"; description="Use Alt-3 to switch between screens"; action = function()
 Keys('F12 3')
   end;
 }
 
 Macro {
-  area="Shell"; key="Alt4"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-4 to switch between screens"; action = function()
+  area="Shell"; key="Alt4"; flags="DisableOutput"; description="Use Alt-4 to switch between screens"; action = function()
 Keys('F12 4')
   end;
 }
 
 Macro {
-  area="Shell"; key="Alt5"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-5 to switch between screens"; action = function()
+  area="Shell"; key="Alt5"; flags="DisableOutput"; description="Use Alt-5 to switch between screens"; action = function()
 Keys('F12 5')
   end;
 }
 
 Macro {
-  area="Shell"; key="Alt6"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-6 to switch between screens"; action = function()
+  area="Shell"; key="Alt6"; flags="DisableOutput"; description="Use Alt-6 to switch between screens"; action = function()
 Keys('F12 6')
   end;
 }
 
 Macro {
-  area="Shell"; key="Alt7"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-7 to switch between screens"; action = function()
+  area="Shell"; key="Alt7"; flags="DisableOutput"; description="Use Alt-7 to switch between screens"; action = function()
 Keys('F12 7')
   end;
 }
 
 Macro {
-  area="Shell"; key="Alt8"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-8 to switch between screens"; action = function()
+  area="Shell"; key="Alt8"; flags="DisableOutput"; description="Use Alt-8 to switch between screens"; action = function()
 Keys('F12 8')
   end;
 }
 
 Macro {
-  area="Shell"; key="Alt9"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-9 to switch between screens"; action = function()
+  area="Shell"; key="Alt9"; flags="DisableOutput"; description="Use Alt-9 to switch between screens"; action = function()
 Keys('F12 9')
   end;
 }
 
 Macro {
-  area="Shell"; key="Alt0"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-0 to switch between screens"; action = function()
+  area="Shell"; key="Alt0"; flags="DisableOutput"; description="Use Alt-0 to switch between screens"; action = function()
 Keys('F12 0')
   end;
 }
 
 Macro {
-  area="Editor"; key="Alt1"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-1 to switch between screens"; action = function()
+  area="Editor"; key="Alt1"; flags="DisableOutput"; description="Use Alt-1 to switch between screens"; action = function()
 Keys('F12 1')
   end;
 }
 
 Macro {
-  area="Editor"; key="Alt2"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-2 to switch between screens"; action = function()
+  area="Editor"; key="Alt2"; flags="DisableOutput"; description="Use Alt-2 to switch between screens"; action = function()
 Keys('F12 2')
   end;
 }
 
 Macro {
-  area="Editor"; key="Alt3"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-3 to switch between screens"; action = function()
+  area="Editor"; key="Alt3"; flags="DisableOutput"; description="Use Alt-3 to switch between screens"; action = function()
 Keys('F12 3')
   end;
 }
 
 Macro {
-  area="Editor"; key="Alt4"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-4 to switch between screens"; action = function()
+  area="Editor"; key="Alt4"; flags="DisableOutput"; description="Use Alt-4 to switch between screens"; action = function()
 Keys('F12 4')
   end;
 }
 
 Macro {
-  area="Editor"; key="Alt5"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-5 to switch between screens"; action = function()
+  area="Editor"; key="Alt5"; flags="DisableOutput"; description="Use Alt-5 to switch between screens"; action = function()
 Keys('F12 5')
   end;
 }
 
 Macro {
-  area="Editor"; key="Alt6"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-6 to switch between screens"; action = function()
+  area="Editor"; key="Alt6"; flags="DisableOutput"; description="Use Alt-6 to switch between screens"; action = function()
 Keys('F12 6')
   end;
 }
 
 Macro {
-  area="Editor"; key="Alt7"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-7 to switch between screens"; action = function()
+  area="Editor"; key="Alt7"; flags="DisableOutput"; description="Use Alt-7 to switch between screens"; action = function()
 Keys('F12 7')
   end;
 }
 
 Macro {
-  area="Editor"; key="Alt8"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-8 to switch between screens"; action = function()
+  area="Editor"; key="Alt8"; flags="DisableOutput"; description="Use Alt-8 to switch between screens"; action = function()
 Keys('F12 8')
   end;
 }
 
 Macro {
-  area="Editor"; key="Alt9"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-9 to switch between screens"; action = function()
+  area="Editor"; key="Alt9"; flags="DisableOutput"; description="Use Alt-9 to switch between screens"; action = function()
 Keys('F12 9')
   end;
 }
 
 Macro {
-  area="Editor"; key="Alt0"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-0 to switch between screens"; action = function()
+  area="Editor"; key="Alt0"; flags="DisableOutput"; description="Use Alt-0 to switch between screens"; action = function()
 Keys('F12 0')
   end;
 }
 
 Macro {
-  area="Viewer"; key="Alt1"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-1 to switch between screens"; action = function()
+  area="Viewer"; key="Alt1"; flags="DisableOutput"; description="Use Alt-1 to switch between screens"; action = function()
 Keys('F12 1')
   end;
 }
 
 Macro {
-  area="Viewer"; key="Alt2"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-2 to switch between screens"; action = function()
+  area="Viewer"; key="Alt2"; flags="DisableOutput"; description="Use Alt-2 to switch between screens"; action = function()
 Keys('F12 2')
   end;
 }
 
 Macro {
-  area="Viewer"; key="Alt3"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-3 to switch between screens"; action = function()
+  area="Viewer"; key="Alt3"; flags="DisableOutput"; description="Use Alt-3 to switch between screens"; action = function()
 Keys('F12 3')
   end;
 }
 
 Macro {
-  area="Viewer"; key="Alt4"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-4 to switch between screens"; action = function()
+  area="Viewer"; key="Alt4"; flags="DisableOutput"; description="Use Alt-4 to switch between screens"; action = function()
 Keys('F12 4')
   end;
 }
 
 Macro {
-  area="Viewer"; key="Alt5"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-5 to switch between screens"; action = function()
+  area="Viewer"; key="Alt5"; flags="DisableOutput"; description="Use Alt-5 to switch between screens"; action = function()
 Keys('F12 5')
   end;
 }
 
 Macro {
-  area="Viewer"; key="Alt6"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-6 to switch between screens"; action = function()
+  area="Viewer"; key="Alt6"; flags="DisableOutput"; description="Use Alt-6 to switch between screens"; action = function()
 Keys('F12 6')
   end;
 }
 
 Macro {
-  area="Viewer"; key="Alt7"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-7 to switch between screens"; action = function()
+  area="Viewer"; key="Alt7"; flags="DisableOutput"; description="Use Alt-7 to switch between screens"; action = function()
 Keys('F12 7')
   end;
 }
 
 Macro {
-  area="Viewer"; key="Alt8"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-8 to switch between screens"; action = function()
+  area="Viewer"; key="Alt8"; flags="DisableOutput"; description="Use Alt-8 to switch between screens"; action = function()
 Keys('F12 8')
   end;
 }
 
 Macro {
-  area="Viewer"; key="Alt9"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-9 to switch between screens"; action = function()
+  area="Viewer"; key="Alt9"; flags="DisableOutput"; description="Use Alt-9 to switch between screens"; action = function()
 Keys('F12 9')
   end;
 }
 
 Macro {
-  area="Viewer"; key="Alt0"; flags="DisableOutput|FilePanels|PluginPanels"; description="Use Alt-0 to switch between screens"; action = function()
+  area="Viewer"; key="Alt0"; flags="DisableOutput"; description="Use Alt-0 to switch between screens"; action = function()
 Keys('F12 0')
   end;
 }
