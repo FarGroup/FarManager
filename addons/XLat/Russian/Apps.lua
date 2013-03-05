100ff55c0719159652f0f68d6192b591b725c9b0
Shmuel Zeigerman <shmuz@013net.net>
2013-03-05 20:48:05 +0000

    Косметика.

--- addons/XLat/Russian/Apps.lua
+++ addons/XLat/Russian/Apps.lua
@@ -1,5 +1,5 @@
 Macro {
-  area="Common"; key="Apps"; description="Use hotkey &lt;Apps&gt; to perform XLat function."; flags="DisableOutput"; action = function()
+  area="Common"; key="Apps"; description="Use hotkey <Apps> to perform XLat function."; flags="DisableOutput"; action = function()
 Keys('xlat')
   end;
 }
--- addons/XLat/Russian/CtrlShiftX.lua
+++ addons/XLat/Russian/CtrlShiftX.lua
@@ -1,5 +1,5 @@
 Macro {
-  area="Common"; key="CtrlShiftX"; description="Use hotkey &lt;Ctrl-Shift-X&gt; to perform XLat function."; flags="DisableOutput"; action = function()
+  area="Common"; key="CtrlShiftX"; description="Use hotkey <Ctrl-Shift-X> to perform XLat function."; flags="DisableOutput"; action = function()
 Keys('xlat')
   end;
 }
