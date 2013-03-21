48b2385d396916882bdbd522495aec6b571742a4
Vladimir Surguchev <vladimir.surguchev@gmail.com>
2013-03-21 11:31:13 +0000

    более 'правильный' макрос

--- addons/Macros/Panel.CtrlShiftBackSlash.lua
+++ addons/Macros/Panel.CtrlShiftBackSlash.lua
@@ -1,6 +1,6 @@
 Macro {
-  area="Shell"; key="CtrlShiftBackSlash"; flags=""; description="Activate the same folder in the passive panel as in the active panel."; action = function()
-Keys('F9 Tab Enter Up Enter Enter')
+  area="Shell"; key="CtrlShiftBackSlash"; flags=""; description="Activate the same folder in the passive panel as in the active panel"; action = function()
+    panel.SetPanelDirectory(nil,0,panel.GetPanelDirectory(nil,1))
   end;
 }
 
