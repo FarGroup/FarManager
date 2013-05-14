8d812dc15bdb248b56271ebb4bf07bf0b57dc9bd
Alex Yaroslavsky <trexinc@gmail.com>
2013-05-14 20:56:13 +0000

    Add PluginGUID for Far 3.0.3410

--- plugins/multiarc/multiarcversion.hpp
+++ plugins/multiarc/multiarcversion.hpp
@@ -2,9 +2,9 @@
 #define __MULTIARCVERSION_HPP__
 #include "farversion.hpp"
 
-#define MA_BUILD 193
+#define MA_BUILD 194
 #define MAPRODUCTNAME "MultiArc"
-#define FMTDESCRIPTION "Second-level plugin module for MultiArc FAR Manager plugin"
+#define FMTDESCRIPTION "Second-level plugin module for MultiArc Far Manager plugin"
 
 #define magenericpluginrc(major, minor, name, filename) fullgenericpluginrc_nobuild(major, minor, FMTDESCRIPTION, name, filename, FARCOPYRIGHT, FAR_MAJOR_VER, FAR_MINOR_VER, MA_BUILD, MAPRODUCTNAME)
 
