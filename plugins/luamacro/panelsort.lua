1ba92dbc4841d1340e8aef0da580aa1c9590389a
Shmuel Zeigerman <shmuz@013net.net>
2013-08-11 17:28:06 +0000

    Уточнение revision 10846.

--- plugins/luamacro/panelsort.lua
+++ plugins/luamacro/panelsort.lua
@@ -49,9 +49,7 @@ end
 
 local C = ffi.C
 local F = far.Flags
-local SM_UNSORTED,SM_NAME,SM_EXT,SM_FULLNAME = F.SM_UNSORTED,F.SM_NAME,F.SM_EXT,F.SM_FULLNAME
 local PPIF_SELECTED = F.PPIF_SELECTED
-local PPIF_PROCESSDESCR = F.PPIF_PROCESSDESCR
 local FILE_ATTRIBUTE_DIRECTORY = 0x00000010
 local MCODE_F_SETCUSTOMSORTMODE = 0x80C68
 local band = bit.band -- 32 bits, be careful
@@ -60,13 +58,15 @@ local tonumber = tonumber
 local CustomSortModes = {} -- key=integer, value=function
 
 -- called from user script
-local function SetCustomSortMode (whatpanel, nMode, fCompare, bInvertByDefault, sIndicator)
+local function SetCustomSortMode (whatpanel, nMode, Settings)
   whatpanel = whatpanel==1 and 1 or 0
   assert(type(nMode)=="number" and nMode==math.floor(nMode) and nMode>=100 and nMode<=0x7FFFFFFF)
-  assert(type(fCompare)=="function")
-  assert(type(sIndicator)=="string")
-  CustomSortModes[nMode] = fCompare
-  return far.MacroCallFar(MCODE_F_SETCUSTOMSORTMODE, whatpanel, nMode, bInvertByDefault, sIndicator)
+  assert(type(Settings)=="table")
+  assert(type(Settings.Compare)=="function")
+  local InvertByDefault = not not Settings.InvertByDefault
+  local Indicator = type(Settings.Indicator)=="string" and Settings.Indicator or ""
+  CustomSortModes[nMode] = Settings
+  return far.MacroCallFar(MCODE_F_SETCUSTOMSORTMODE, whatpanel, nMode, InvertByDefault, Indicator)
 end
 
 ffi.cdef[[
@@ -76,14 +76,14 @@ ffi.cdef[[
     const struct FileListItem **Data;
     size_t                      DataSize;
     void                      (*FileListToPluginItem)(const struct FileListItem*, struct PluginPanelItem*);
-    int                         ListSortGroups;
-    int                         ListSelectedFirst;
-    int                         ListDirectoriesFirst;
-    int                         ListSortMode;
+    int                         SortGroups;
+    int                         SelectedFirst;
+    int                         DirectoriesFirst;
+    int                         SortMode;
     int                         RevertSorting;
     int                         ListPanelMode; // currently not used
-    int                         ListNumericSort;
-    int                         ListCaseSensitiveSort;
+    int                         NumericSort;
+    int                         CaseSensitiveSort;
     HANDLE                      hSortPlugin;
   } CustomSort;
 ]]
@@ -99,13 +99,46 @@ local DOTS = Utf16Buf("..")
 
 -- called from Far
 local function SortPanelItems (params)
-  jit.flush()
+--local timeStart = Far.UpTime
+--jit.flush()
   params = ffi.cast("CustomSort*", params)
-  local fCompare = CustomSortModes[tonumber(params.ListSortMode)]
-  if not fCompare then return end
+  local tSettings = CustomSortModes[tonumber(params.SortMode)]
+  if not tSettings then return end
+  local Compare = tSettings.Compare
 
   local pi1 = ffi.new("struct PluginPanelItem")
   local pi2 = ffi.new("struct PluginPanelItem")
+
+  local SortGroups, SelectedFirst, DirectoriesFirst, RevertSorting
+
+  if tSettings.SortGroups==0 then SortGroups=false
+  elseif tSettings.SortGroups==1 then SortGroups=true
+  else SortGroups = params.SortGroups ~= 0
+  end
+
+  if tSettings.SelectedFirst==0 then SelectedFirst=false
+  elseif tSettings.SelectedFirst==1 then SelectedFirst=true
+  else SelectedFirst = params.SelectedFirst ~= 0
+  end
+
+  if tSettings.DirectoriesFirst==0 then DirectoriesFirst=false
+  elseif tSettings.DirectoriesFirst==1 then DirectoriesFirst=true
+  else DirectoriesFirst = params.DirectoriesFirst ~= 0
+  end
+
+  if tSettings.RevertSorting==0 then RevertSorting=false
+  elseif tSettings.RevertSorting==1 then RevertSorting=true
+  else RevertSorting = params.RevertSorting ~= 0
+  end
+
+  local outParams = {
+    SortGroups        = params.SortGroups ~= 0;
+    SelectedFirst     = params.SelectedFirst ~= 0;
+    DirectoriesFirst  = params.DirectoriesFirst ~= 0;
+    RevertSorting     = params.RevertSorting ~= 0;
+    NumericSort       = params.NumericSort ~= 0;
+    CaseSensitiveSort = params.CaseSensitiveSort ~= 0;
+  }
 
   local function Before(e1, e2)
     params.FileListToPluginItem(e1, pi1)
@@ -125,36 +158,27 @@ local function SortPanelItems (params)
       return false
     end
     ----------------------------------------------------------------------------
-    if params.ListSortMode == SM_UNSORTED then
-      if params.ListSelectedFirst ~= 0 then
-        local s1,s2 = band(tonumber(pi1.Flags),PPIF_SELECTED), band(tonumber(pi2.Flags),PPIF_SELECTED)
-        if s1 ~= s2 then return s1 > s2 end --> TODO: fix in Far
-      end
-      if params.RevertSorting ~= 0 then pi1,pi2 = pi2,pi1 end
-      return pi1.Reserved[0] < pi2.Reserved[0] -- Reserved[0] contains 'Position'
-    end
+    -- return pi1.Reserved[0] < pi2.Reserved[0] -- Reserved[0] contains 'Position'
     ----------------------------------------------------------------------------
-    if params.ListDirectoriesFirst then
+    if DirectoriesFirst then
       local a1,a2 = band(tonumber(pi1.FileAttributes), FILE_ATTRIBUTE_DIRECTORY),
                     band(tonumber(pi2.FileAttributes), FILE_ATTRIBUTE_DIRECTORY)
       if a1 ~= a2 then return a1 > a2 end
     end
     ----------------------------------------------------------------------------
-    if params.ListSelectedFirst ~= 0 then
+    if SelectedFirst then
       local s1,s2 = band(tonumber(pi1.Flags),PPIF_SELECTED), band(tonumber(pi2.Flags),PPIF_SELECTED)
       if s1 ~= s2 then return s1 > s2 end
     end
     ----------------------------------------------------------------------------
-    if params.ListSortGroups and
-      (params.ListSortMode==SM_NAME or params.ListSortMode==SM_EXT or params.ListSortMode==SM_FULLNAME)
-    then
+    if SortGroups then
        -- Reserved[1] contains 'SortGroup'
       if pi1.Reserved[1] ~= pi2.Reserved[1] then return pi1.Reserved[1] < pi2.Reserved[1] end
     end
     ----------------------------------------------------------------------------
-    local r = fCompare(pi1,pi2)
+    local r = Compare(pi1, pi2, outParams)
     if r ~= 0 then
-      if params.RevertSorting ~= 0 then r = -r end
+      if RevertSorting then r = -r end
       return r < 0
     else
       return C._wcsicmp(pi1.FileName,pi2.FileName) < 0
@@ -164,6 +188,7 @@ local function SortPanelItems (params)
   shellsort(params.Data, tonumber(params.DataSize), Before)
   -- qsort(params.Data, 0, tonumber(params.DataSize)-1, Before)
 
+--far.Message(Far.UpTime - timeStart)
   return true
 end
 
