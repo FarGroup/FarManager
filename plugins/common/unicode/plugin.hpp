b33f27cf5f5791f4c8511b1a93411c95b3d9dc59
Shmuel Zeigerman <shmuz@013net.net>
2013-09-15 18:04:41 +0000

    update headers to 3652

--- plugins/common/unicode/plugin.hpp
+++ plugins/common/unicode/plugin.hpp
@@ -5,7 +5,7 @@
 /*
   plugin.hpp
 
-  Plugin API for Far Manager 3.0 build 3648
+  Plugin API for Far Manager 3.0 build 3652
 */
 
 /*
@@ -43,7 +43,7 @@ other possible license with no implications from the above license on them.
 #define FARMANAGERVERSION_MAJOR 3
 #define FARMANAGERVERSION_MINOR 0
 #define FARMANAGERVERSION_REVISION 0
-#define FARMANAGERVERSION_BUILD 3648
+#define FARMANAGERVERSION_BUILD 3652
 #define FARMANAGERVERSION_STAGE VS_RELEASE
 
 #ifndef RC_INVOKED
@@ -1112,28 +1112,6 @@ struct FarMacroValue
 	Value
 #endif
 	;
-};
-
-enum MACROPLUGINRETURNTYPE
-{
-	MPRT_NORMALFINISH  = 0,
-	MPRT_ERRORFINISH   = 1,
-	MPRT_ERRORPARSE    = 2,
-	MPRT_KEYS          = 3,
-	MPRT_PRINT         = 4,
-	MPRT_PLUGINCALL    = 5,
-	MPRT_PLUGINMENU    = 6,
-	MPRT_PLUGINCONFIG  = 7,
-	MPRT_PLUGINCOMMAND = 8,
-	MPRT_USERMENU      = 9,
-	MPRT_COMMONCASE    = 100
-};
-
-struct MacroPluginReturn
-{
-	size_t Count;
-	struct FarMacroValue *Values;
-	enum MACROPLUGINRETURNTYPE ReturnType;
 };
 
 struct FarMacroCall
@@ -2455,12 +2433,34 @@ enum MACROCALLTYPE
 	MCT_GETCUSTOMSORTMODES = 13,
 };
 
+enum MACROPLUGINRETURNTYPE
+{
+	MPRT_NORMALFINISH  = 0,
+	MPRT_ERRORFINISH   = 1,
+	MPRT_ERRORPARSE    = 2,
+	MPRT_KEYS          = 3,
+	MPRT_PRINT         = 4,
+	MPRT_PLUGINCALL    = 5,
+	MPRT_PLUGINMENU    = 6,
+	MPRT_PLUGINCONFIG  = 7,
+	MPRT_PLUGINCOMMAND = 8,
+	MPRT_USERMENU      = 9,
+	MPRT_COMMONCASE    = 10,
+};
+
+struct MacroPluginReturn
+{
+	intptr_t ReturnType;
+	size_t Count;
+	struct FarMacroValue *Values;
+};
+
 struct OpenMacroPluginInfo
 {
-	size_t StructSize;
 	enum MACROCALLTYPE CallType;
-	HANDLE Handle;
+	intptr_t Handle;
 	struct FarMacroCall *Data;
+	struct MacroPluginReturn Ret;
 };
 
 
