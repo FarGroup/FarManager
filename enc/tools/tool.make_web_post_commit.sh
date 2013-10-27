06408cbc4215bc0b1c9396f9a9bb3797af87573a
Alex Yaroslavsky <trexinc@gmail.com>
2013-10-27 15:04:21 +0000

    add lock

--- enc/tools/tool.make_web_post_commit.sh
+++ enc/tools/tool.make_web_post_commit.sh
@@ -1,19 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 #
 # For use as a post commit hook from webservice
 # $1 = path to repository
 # $2 = number of revision commited
 #
 
-svn log -r "$2" -v "$1" | /bin/grep trunk/enc
-if [ $? -ne 0 ]; then exit 0; fi
+( 
+	flock 200
 
-svn co -q --force $1/trunk/enc/ /var/tmp/enc
+	svn log -r "$2" -v "$1" | grep trunk/enc
+	if [ $? -ne 0 ]; then exit 0; fi
 
-cd /var/tmp/enc/tools && python ./tool.make_inet.py
-if [ $? -ne 0 ]; then exit 0; fi
-cd /
+	svn co -q --force $1/trunk/enc/ /var/tmp/enc
 
-rm -Rf /var/www/api/*
-cp -Rf /var/tmp/enc/build/inet/* /var/www/api/
-rm -Rf /var/tmp/enc
+	cd /var/tmp/enc/tools && python ./tool.make_inet.py
+	if [ $? -ne 0 ]; then exit 0; fi
+	cd /
+
+	rm -Rf /var/www/api/*
+	cp -Rf /var/tmp/enc/build/inet/* /var/www/api/
+	rm -Rf /var/tmp/enc 
+
+) 200>/var/tmp/tool.make_web_post_commit.lock
