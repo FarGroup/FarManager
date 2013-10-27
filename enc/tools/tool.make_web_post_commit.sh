95370ba3d75345f9662b4563b9e1c9c16b11ca35
Alex Yaroslavsky <trexinc@gmail.com>
2013-10-27 14:38:29 +0000

    fix for remote repo

--- enc/tools/tool.make_web_post_commit.sh
+++ enc/tools/tool.make_web_post_commit.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 #
-# For use as a post commit hook in svn (environment not set)
+# For use as a post commit hook from webservice
 # $1 = path to repository
 # $2 = number of revision commited
 #
 
-/usr/bin/svnlook changed "$1" -r "$2" | /bin/grep trunk/enc
+svn log -r "$2" -v "$1" | /bin/grep trunk/enc
 if [ $? -ne 0 ]; then exit 0; fi
 
-/usr/bin/svn co -q --force $1/trunk/enc/ /var/tmp/enc
+svn co -q --force $1/trunk/enc/ /var/tmp/enc
 
-# need to set up path as the script will try to run svn
-export PATH=$PATH:/usr/bin
-cd /var/tmp/enc/tools && /usr/bin/python ./tool.make_inet.py
+cd /var/tmp/enc/tools && python ./tool.make_inet.py
 if [ $? -ne 0 ]; then exit 0; fi
 cd /
 
-/bin/rm -Rf /var/www/api/*
-/bin/cp -Rf /var/tmp/enc/build/inet/* /var/www/api/
-/bin/rm -Rf /var/tmp/enc
+rm -Rf /var/www/api/*
+cp -Rf /var/tmp/enc/build/inet/* /var/www/api/
+rm -Rf /var/tmp/enc
