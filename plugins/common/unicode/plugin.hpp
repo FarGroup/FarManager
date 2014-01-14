ed7ce9620f3416a1c0d9ee9e18b97c7d54afddde
Shmuel Zeigerman <shmuz@013net.net>
2014-01-14 09:23:23 +0000

    update headers to 3770

--- plugins/common/unicode/plugin.hpp
+++ plugins/common/unicode/plugin.hpp
@@ -5,7 +5,7 @@
 /*
   plugin.hpp
 
-  Plugin API for Far Manager 3.0 build 3714
+  Plugin API for Far Manager 3.0 build 3770
 */
 
 /*
@@ -43,7 +43,7 @@ other possible license with no implications from the above license on them.
 #define FARMANAGERVERSION_MAJOR 3
 #define FARMANAGERVERSION_MINOR 0
 #define FARMANAGERVERSION_REVISION 0
-#define FARMANAGERVERSION_BUILD 3714
+#define FARMANAGERVERSION_BUILD 3770
 #define FARMANAGERVERSION_STAGE VS_RELEASE
 
 #ifndef RC_INVOKED
@@ -84,6 +84,16 @@ struct FarColor
 	COLORREF ForegroundColor;
 	COLORREF BackgroundColor;
 	void* Reserved;
+
+#ifdef __cplusplus
+	bool operator ==(const FarColor& rhs) const
+	{
+		return Flags == rhs.Flags
+			&& ForegroundColor == rhs.ForegroundColor
+			&& BackgroundColor == rhs.BackgroundColor
+			&& Reserved == rhs.Reserved;
+	}
+#endif
 };
 
 #define INDEXMASK 0x0000000f
@@ -2171,7 +2181,7 @@ struct MacroPluginReturn
 };
 
 typedef intptr_t (WINAPI *FARAPICALLFAR)(intptr_t CheckCode, struct FarMacroCall* Data);
-typedef void (WINAPI *FARAPICALLPLUGIN)(struct MacroPluginReturn* Data, struct FarMacroCall* Target);
+typedef void (WINAPI *FARAPICALLPLUGIN)(struct MacroPluginReturn* Data, struct FarMacroCall** Target, int* Boolean);
 
 struct MacroPrivateInfo
 {
