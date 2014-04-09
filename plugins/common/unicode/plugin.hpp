36f4ee2ce28c737dea2d9126590841141fb99c3c
Vadim Yegorov <oo.zg.oo@gmail.com>
2014-04-09 15:00:49 +0000

    update headers to 3856

--- plugins/common/unicode/plugin.hpp
+++ plugins/common/unicode/plugin.hpp
@@ -5,7 +5,7 @@
 /*
   plugin.hpp
 
-  Plugin API for Far Manager 3.0 build 3854
+  Plugin API for Far Manager 3.0 build 3856
 */
 
 /*
@@ -43,7 +43,7 @@ other possible license with no implications from the above license on them.
 #define FARMANAGERVERSION_MAJOR 3
 #define FARMANAGERVERSION_MINOR 0
 #define FARMANAGERVERSION_REVISION 0
-#define FARMANAGERVERSION_BUILD 3854
+#define FARMANAGERVERSION_BUILD 3856
 #define FARMANAGERVERSION_STAGE VS_RELEASE
 
 #ifndef RC_INVOKED
@@ -341,7 +341,6 @@ enum FARMESSAGE
 	DN_DROPDOWNOPENED               = 4119,
 	DN_DRAWDLGITEMDONE              = 4120,
 
-	DN_ACTIVATEAPP                  = 0x3FFF,
 	DM_USER                         = 0x4000,
 
 };
