6c28238411dff0a06bea47ac15bd13361ebdb78e
Shmuel Zeigerman <shmuz@013net.net>
2014-04-29 14:47:30 +0000

    Empty message

--- plugins/luamacro/luamacro.lua
+++ plugins/luamacro/luamacro.lua
@@ -264,18 +264,18 @@ local function ExecString (lang, text, params)
 end
 
 local function ProcessCommandLine (CmdLine)
-  local op, text = CmdLine:match("%s*(%w+:)%s*(.-)%s*$")
+  local op, text = CmdLine:match("^%s*(%w+:)%s*(.-)%s*$")
   op = op:lower()
   if op == "lm:" or op == "macro:" then
-    if text ~= "" then
-      text = regex.match(text:lower(), "^(\\w+)(?:\\s|$)")
-      if text == "load" then far.MacroLoadAll()
-      elseif text == "save" then utils.WriteMacros()
-      elseif text == "unload" then utils.UnloadMacros()
-      else ErrMsg(M.CL_UnsupportedCommand .. text)
-      end
-    end
-  elseif op == "lua:" or op == "moon:" then
+    local cmd = text:match("%S*"):lower()
+    if cmd == "load" then far.MacroLoadAll()
+    elseif cmd == "save" then utils.WriteMacros()
+    elseif cmd == "unload" then utils.UnloadMacros()
+    elseif cmd == "post" then -- DEPRECATED, to be removed on 2014-Oct-29.
+      op, text = "lua:", text:match("%S+%s*(.*)")
+    elseif cmd ~= "" then ErrMsg(M.CL_UnsupportedCommand .. cmd) end
+  end
+  if op == "lua:" or op == "moon:" then
     if text~="" then
       local fname, params = SplitMacroString(text)
       if fname then
