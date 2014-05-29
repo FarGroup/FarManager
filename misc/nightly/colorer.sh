90140aa907282bdee2ca587c2a7d5cb0eb5c8fef
Alex Yaroslavsky <trexinc@gmail.com>
2014-05-29 07:33:37 +0000

    ant bug still present

--- misc/nightly/colorer.sh
+++ misc/nightly/colorer.sh
@@ -34,6 +34,9 @@ function bcolorer {
 cd Colorer-schemes || exit 1
 git pull || exit 1
 
+#neweset ubuntu ant 1.8.2/1.9.3 has a bug and can't find the resolver, 1.8.4 works fine
+PATH=~/apache-ant-1.8.4/bin:$PATH
+export PATH
 rm -fR build
 ./build.sh base.far.clean
 ./build.sh base.far &> ../logs/colorerschemes || exit 1
