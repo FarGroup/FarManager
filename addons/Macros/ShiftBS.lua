0baf7b411044093f2975683c9d3383badde08328
Shmuel Zeigerman <shmuz@013net.net>
2014-06-02 20:52:45 +0000

    Расширение списка областей действия.

--- addons/Macros/ShiftBS.lua
+++ addons/Macros/ShiftBS.lua
@@ -1,5 +1,5 @@
 Macro {
-  area="Dialog Editor Shell"; key="ShiftBS"; description="Converts the word before cursor using XLat function";
-  condition = function() return not (Area.Shell and CmdLine.Empty) end;
+  area="Dialog Editor Shell QView Tree Info"; key="ShiftBS"; description="Converts the word before cursor using XLat function";
+  condition = function() return Area.Dialog or Area.Editor or not CmdLine.Empty end;
   action = function() Keys('CtrlShiftLeft XLat CtrlRight') end;
 }
