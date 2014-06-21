c72d696293421918d5f2a6a87f0c997681950202
Shmuel Zeigerman <shmuz@013net.net>
2014-06-21 22:38:02 +0000

    Уточнения.

--- addons/Macros/Edit.OpenURL.lua
+++ addons/Macros/Edit.OpenURL.lua
@@ -1,13 +1,9 @@
 ﻿local patt = regex.new( [=[
   \b http s?  :\/\/ [^\s`'"[\](){}<>]+ |
   \b ftp      :\/\/ [^\s`'"[\](){}<>]+ |
-  \b www     \.     [^\s`'"[\](){}<>]+ |
+  \b www \d*  \.    [^\s`'"[\](){}<>]+ |
   \b mailto:        [^\s`'"[\](){}<>]+ |
-  (?: \b [a-z]:)?
-    (?:
-      (?: [\\\/][\w.\-]+ )+ |
-      [\w.\-]+
-    )
+  (?: \b [a-z]:)? (?: [\\\/]? [\w.\-]+ )+ [\\\/]?
 ]=], "ix")
 
 Macro {
@@ -17,13 +13,17 @@ Macro {
     local s=editor.GetStringW()
     if not s then return end
     local pos = editor.GetInfo().CurPos
-    if pos > s.StringLength then return end
+    if pos > s.StringLength+1 then return end
+    if pos == s.StringLength+1 then pos = pos-1 end
     local text, start = s.StringText.."\0", 1
     while true do
       local b,e = patt:findW(text,start)
+      --far.Show(b,e)
       if b == nil or b > pos then break end
       if e >= pos then
-        win.ShellExecute(nil, "open", win.Utf16ToUtf8(win.subW(text,b,e)))
+        local url = win.Utf16ToUtf8(win.subW(text,b,e))
+        --far.Show(url)
+        win.ShellExecute(nil, "open", url)
         break
       end
       start = e+1
