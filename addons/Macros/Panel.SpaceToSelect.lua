8cc1db51431385f2bf5b723d0855ac26396628ed
Alex Alabuzhev <alabuzhev@gmail.com>
2015-05-20 16:44:33 +0000

    Panel.SpaceToSelect: fix for hidden panels

--- addons/Macros/Panel.SpaceToSelect.lua
+++ addons/Macros/Panel.SpaceToSelect.lua
@@ -1,6 +1,6 @@
 Macro {
   area="Shell"; key="Space"; flags="EmptyCommandLine"; description="Panel: Use Space to select files"; action = function()
-Keys('Ins')
+if APanel.Visible or PPanel.Visible then Keys("Ins") else Keys("Space") end
   end;
 }
 
