3154ae52f53e87f8e73b6e2cebeeed510839da8f
Alex Alabuzhev <alabuzhev@gmail.com>
2015-07-01 06:11:27 +0000

    build fix

--- misc/nightly/netbox.32.bat
+++ misc/nightly/netbox.32.bat
@@ -6,7 +6,7 @@ set TERM=dumb
 cd libs\openssl
 rm -rf out32dll tmp32dll tmp32 inc32 out32
 rm -rf x86
-perl Configure VC-WIN32 no-asm enable-static-engine no-shared no-hw no-camellia no-seed no-mdc2 no-rc4 no-rc5 no-cms no-krb5 no-ec no-whirlpool no-srp no-gost no-idea no-ripemd no-des -Ox -Ob1 -Oi -Os -Oy -GF -GS- -Gy -DNDEBUG;BN_ASM;OPENSSL_NO_CAPIENG;NO_CHMOD;OPENSSL_NO_DGRAM;OPENSSL_NO_RIJNDAEL;DSO_WIN32
+perl Configure VC-WIN32 no-asm enable-static-engine no-shared no-hw no-camellia no-seed no-rc4 no-rc5 no-cms no-krb5 no-whirlpool no-srp no-gost no-idea no-ripemd -Ox -Ob1 -Oi -Os -Oy -GF -GS- -Gy -DNDEBUG;BN_ASM;OPENSSL_NO_CAPIENG;NO_CHMOD;OPENSSL_NO_DGRAM;OPENSSL_NO_RIJNDAEL;DSO_WIN32
 call ms\do_ms
 nmake -f ms\nt.mak
 mkdir x86
--- misc/nightly/netbox.64.bat
+++ misc/nightly/netbox.64.bat
@@ -6,7 +6,7 @@ set TERM=dumb
 cd libs\openssl
 rm -rf out32dll tmp32dll tmp32 inc32 out32
 rm -rf x64
-perl Configure VC-WIN64A no-asm enable-static-engine no-shared no-hw no-camellia no-seed no-mdc2 no-rc4 no-rc5 no-cms no-krb5 no-ec no-whirlpool no-srp no-gost no-idea no-ripemd no-des -Ox -Ob1 -Oi -Os -Oy -GF -GS- -Gy  -DNDEBUG;OPENSSL_NO_CAPIENG;NO_CHMOD;OPENSSL_NO_DGRAM;OPENSSL_NO_RIJNDAEL;DSO_WIN32
+perl Configure VC-WIN64A no-asm enable-static-engine no-shared no-hw no-camellia no-seed no-rc4 no-rc5 no-cms no-krb5 no-whirlpool no-srp no-gost no-idea no-ripemd -Ox -Ob1 -Oi -Os -Oy -GF -GS- -Gy  -DNDEBUG;OPENSSL_NO_CAPIENG;NO_CHMOD;OPENSSL_NO_DGRAM;OPENSSL_NO_RIJNDAEL;DSO_WIN32
 call ms\do_win64a
 nmake -f ms\nt.mak
 mkdir x64
