fcd35c517326b9275b386f4b27bda678fd036064
Alex Alabuzhev <alabuzhev@gmail.com>
2015-10-23 17:50:29 +0000

    0003097: NetBox - корректировка скрипта сборки

--- misc/nightly/netbox.32.bat
+++ misc/nightly/netbox.32.bat
@@ -3,17 +3,7 @@ echo on
 set PATH=%PATH%;c:\perl\perl\site\bin;c:\perl\perl\bin;c:\perl\c\bin;C:\unxutils\usr\local\wbin
 set TERM=dumb
 
-cd libs\openssl
-rm -rf out32dll tmp32dll tmp32 inc32 out32
-rm -rf x86
-perl Configure VC-WIN32 no-unit-test no-cast no-err no-bf no-sctp no-rsax no-asm enable-static-engine no-shared no-hw no-camellia no-seed no-rc4 no-rc5 no-krb5 no-whirlpool no-srp no-gost no-idea no-ripemd -Ox -Ob1 -Oi -Os -Oy -GF -GS- -Gy -DNDEBUG;OPENSSL_NO_CAPIENG;NO_CHMOD;OPENSSL_NO_DGRAM;OPENSSL_NO_RIJNDAEL;DSO_WIN32
-call ms\do_ms
-nmake -f ms\nt.mak
-mkdir x86
-cp out32/ssleay32.lib out32/libeay32.lib x86
-cp -R inc32 x86
-
-cd ..\..\build\Release\x86
+cd build\Release\x86
 
 set FAR_VERSION=Far3
 set PROJECT_ROOT=c:\src\Far-NetBox
--- misc/nightly/netbox.64.bat
+++ misc/nightly/netbox.64.bat
@@ -3,17 +3,7 @@ echo on
 set PATH=%PATH%;c:\perl\perl\site\bin;c:\perl\perl\bin;c:\perl\c\bin;C:\unxutils\usr\local\wbin
 set TERM=dumb
 
-cd libs\openssl
-rm -rf out32dll tmp32dll tmp32 inc32 out32
-rm -rf x64
-perl Configure VC-WIN64A no-unit-test no-cast no-err no-bf no-sctp no-rsax no-asm enable-static-engine no-shared no-hw no-camellia no-seed no-rc4 no-rc5 no-krb5 no-whirlpool no-srp no-gost no-idea no-ripemd -Ox -Ob1 -Oi -Os -Oy -GF -GS- -Gy  -DNDEBUG;OPENSSL_NO_CAPIENG;NO_CHMOD;OPENSSL_NO_DGRAM;OPENSSL_NO_RIJNDAEL;DSO_WIN32
-call ms\do_win64a
-nmake -f ms\nt.mak
-mkdir x64
-cp out32/ssleay32.lib out32/libeay32.lib x64
-cp -R inc32 x64
-
-cd ..\..\build\Release\x64
+cd build\Release\x64
 
 set FAR_VERSION=Far3
 set PROJECT_ROOT=c:\src\Far-NetBox
