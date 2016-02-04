2258e5bc5e043e6034baf8dc6321742a1ea6f9ac
Alex Alabuzhev <alabuzhev@gmail.com>
2016-02-04 19:33:06 +0000

    More black magic to support W2k.

--- plugins/common/vc_crt_fix.asm
+++ plugins/common/vc_crt_fix.asm
@@ -35,6 +35,12 @@ EncodePointerWrapper proto stdcall :dword
 DecodePointerWrapper proto stdcall :dword
 GetModuleHandleExWWrapper proto stdcall :dword, :dword, :dword
 InitializeSListHeadWrapper proto stdcall :dword
+InterlockedFlushSListWrapper proto stdcall :dword
+InterlockedPopEntrySListWrapper proto stdcall :dword
+InterlockedPushEntrySListWrapper proto stdcall :dword, :dword
+InterlockedPushListSListExWrapper proto stdcall :dword, :dword, :dword, :dword
+RtlFirstEntrySListWrapper proto stdcall :dword
+QueryDepthSListWrapper proto stdcall :dword
 
 .const
 align 4
@@ -42,10 +48,22 @@ __imp__EncodePointer@4 dd EncodePointerWrapper
 __imp__DecodePointer@4 dd DecodePointerWrapper
 __imp__GetModuleHandleExW@12 dd GetModuleHandleExWWrapper
 __imp__InitializeSListHead@4 dd InitializeSListHeadWrapper
+__imp__InterlockedFlushSList@4 dd InterlockedFlushSListWrapper
+__imp__InterlockedPopEntrySList@4 dd InterlockedPopEntrySListWrapper
+__imp__InterlockedPushEntrySList@8 dd InterlockedPushEntrySListWrapper
+__imp__InterlockedPushListSListEx@16 dd InterlockedPushListSListExWrapper
+__imp__RtlFirstEntrySList@4 dd RtlFirstEntrySListWrapper
+__imp__QueryDepthSList@4 dd QueryDepthSListWrapper
 
 public \
 __imp__EncodePointer@4,
 __imp__DecodePointer@4,
 __imp__GetModuleHandleExW@12,
-__imp__InitializeSListHead@4
+__imp__InitializeSListHead@4,
+__imp__InterlockedFlushSList@4,
+__imp__InterlockedPopEntrySList@4,
+__imp__InterlockedPushEntrySList@8,
+__imp__InterlockedPushListSListEx@16,
+__imp__RtlFirstEntrySList@4,
+__imp__QueryDepthSList@4
 end
