bbab4836fbcc12a648a2d7dcfbd5a77570ffef5c
Alex Alabuzhev <alabuzhev@gmail.com>
2016-10-09 19:12:13 +0000

    simplified svn_tag_build.bat/m4

--- far/svn_tag_build.bat
+++ far/svn_tag_build.bat
@@ -1,44 +1,40 @@
 @echo off
 setlocal
-set command=tools\m4 -P svn_tag_build.m4
 
-if "%~1"=="" set mode=info && %command% && goto pause
+for /f "tokens=1-3" %%i in ('tools\m4 -P svn_tag_build.m4') do (
+set major=%%i
+set minor=%%j
+set build=%%k
+)
 
-set tag=%1%2_b%3
-goto %mode%
+set tag=%major%%minor%_b%build%
 
-:info
 echo --------------------------------------------------------------------
 echo Continue only if you are sure that you have set the correct
 echo build and commited the changes.
-echo This command will tag the trunk under tag/%tag%.
+echo This command will tag the trunk under tag/unicode_far/%tag%.
 echo --------------------------------------------------------------------
 echo If you're not sure press CtrlC.
 echo --------------------------------------------------------------------
 echo --------------------------------------------------------------------
 echo Продолжайте только если вы уверены, что вы выставили правильный
 echo номер билда и закоммитили изменения.
-echo Эта команда пометит текущий trunk в tags/%tag%.
+echo Эта команда пометит текущий trunk в tags/unicode_far/%tag%.
 echo --------------------------------------------------------------------
 echo Если вы не уверены, то нажмите CtrlC
 echo --------------------------------------------------------------------
-goto :eof
-
-:pause
 pause
 echo.
 
-set mode=tag && %command% && goto :eof
-
-:tag
 for /f "tokens=3" %%f in ('svn info ^| find "Root:"') do set repo=%%f
+
 set tag_path=%repo%/tags/unicode_far/%tag%
 
 svn info %tag_path% > nul 2>&1 & (
 	if not errorlevel 1 (
 		echo Error: tag %tag% already exists
 	) else (
-		svn copy %repo%/trunk/unicode_far %tag_path% -m "tag build %3"
+		svn copy %repo%/trunk/unicode_far %tag_path% -m "tag build %build%"
 	)
 )
 endlocal
--- far/svn_tag_build.m4
+++ far/svn_tag_build.m4
@@ -1,2 +1,2 @@
 m4_include(`farversion.m4')m4_dnl
-m4_esyscmd(`svn_tag_build 'MAJOR` 'MINOR` 'BUILD)m4_dnl
+m4_esyscmd(`echo 'MAJOR` 'MINOR` 'BUILD)m4_dnl
