d31802388dfd6bb82b78e09f71fd6d906f260bde
Alex Yaroslavsky <alexy@forescout.com>
2017-12-11 14:55:55 +0200

    fix build of docs and enc

--- misc/nightly/docs.sh
+++ misc/nightly/docs.sh
@@ -2,16 +2,23 @@
 
 function makedocs2 {
 
-mkdir -p $1/Documentation
+mkdir -p $1/Documentation/eng
+mkdir -p $1/Documentation/rus
+mkdir -p $1/Addons
 
 ( \
-	svn export file://`pwd`/syncrepo/trunk/docs/ENG $1/Documentation/eng && \
-	svn export file://`pwd`/syncrepo/trunk/docs/RUS $1/Documentation/rus && \
-	svn export file://`pwd`/syncrepo/trunk/addons $1/Addons && \
+	cp -R far.git/docs/ENG/*  $1/Documentation/eng/ && \
+	cp -R far.git/docs/RUS/* $1/Documentation/rus/ && \
+	cp -R far.git/addons/* $1/Addons/ && \
 
-	svn export file://`pwd`/syncrepo/trunk/docs/RestoreOldPluginSettings.cmd $1/ && \
-	svn export file://`pwd`/syncrepo/trunk/docs/SaveOldPluginSettings.cmd $1/ \
+	cp -R far.git/docs/RestoreOldPluginSettings.cmd $1/ && \
+	cp -R far.git/docs/SaveOldPluginSettings.cmd $1/ \
 ) || return 1
+
+find $1/Documentation -type f -exec unix2dos {} \;
+find $1/Addons -type f -exec unix2dos {} \;
+unix2dos $1/RestoreOldPluginSettings.cmd
+unix2dos $1/SaveOldPluginSettings.cmd
 
 }
 
