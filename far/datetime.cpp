eb2486f0487b2bf153d67d942caecdfe87f7e313
Vladimir Surguchev <vladimir.surguchev@gmail.com>
2018-05-04 15:41:00 +0300

    креш при парсинге даты из пробелов, если сераратор даты тоже пробел.

--- far/datetime.cpp
+++ far/datetime.cpp
@@ -531,7 +531,7 @@ string MkStrFTime(const wchar_t *Format)
 
 void ParseDateComponents(const string& Src, const range<WORD*>& Dst, wchar_t Separator, WORD Default)
 {
-	const auto Components = enum_tokens(Src, string_view(&Separator, 1));
+	const auto Components = enum_tokens(trim(Src), string_view(&Separator, 1));
 	std::transform(ALL_CONST_RANGE(Components), Dst.begin(), [&](const string_view i)
 	{
 		const auto Str = trim(i);
