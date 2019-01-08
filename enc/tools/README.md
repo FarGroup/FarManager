65338c89fd4cf7eaff8ffa275753ee5b3177972b
Alex Yaroslavsky <alex.yaroslavsky@forescout.com>
2019-01-08 19:13:49 +0200

    fix garbled index tab in russian enc chm built in appveyor

--- enc/tools/README.md
+++ enc/tools/README.md
@@ -3,9 +3,10 @@ Output is created in `/enc/build` of the project source tree.
 
 Usage:
 
-    python tool.make_chm.py
+tool.make_enc_chm.bat
+tool.make_lua_chm.bat
 
-Then change dir to `../build/chm` and build .hhp project file
-with HCW or `hhc.exe`  Use `clean.py` to remove created files.
+Results will be under `../build`.
+Use `clean.py` to remove created files.
 
 See also [.\contrib\*](contrib).
--- enc/tools/tool.make_enc_chm.bat
+++ enc/tools/tool.make_enc_chm.bat
@@ -3,7 +3,8 @@ cd "%~dp0" || exit 1
 python clean.py || exit 1
 python tool.make_chm.py || exit 1
 cd ../build/chm/ru || exit 1
-"C:\Program Files (x86)\HTML Help Workshop\hhc.exe" pluginsr.hhp
+rem use chmcmd to build russian enc, it will always generate a chm file as if the system locale is russian
+"%~dp0chmcmd\chmcmd.exe" pluginsr.hhp
 if not exist FarEncyclopedia.ru.chm (
 	echo "Error: FarEncyclopedia.ru.chm wasn't created!"
 	exit 1
