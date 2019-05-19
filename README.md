7954993ca58fe578f303bb3ba6f4d635adac023d
Alex Alabuzhev <alabuzhev@gmail.com>
2019-05-19 12:47:45 +0100

    Add Polish translation to installer, minor travis changes

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ https://www.farmanager.com
 | | AppVeyor | Travis | Azure |
 |-|-|-|-|
 | VS | [![AppVeyor](https://img.shields.io/appveyor/ci/FarGroup/farmanager.svg?logo=appveyor)](https://ci.appveyor.com/project/FarGroup/farmanager/history) | [![Travis CI](https://img.shields.io/travis/FarGroup/FarManager.svg?logo=travis)](https://travis-ci.com/FarGroup/FarManager/builds) | [![Azure Pipelines](https://img.shields.io/azure-devops/build/FarGroup/66d0ddcf-a098-4b98-9470-1c90632c4ba3/1.svg?logo=azuredevops)](https://dev.azure.com/FarGroup/FarManager/_build?definitionId=1) |
-| GCC | [![AppVeyor](https://img.shields.io/appveyor/ci/FarGroup/farmanager-5lhsj.svg?logo=appveyor)](https://ci.appveyor.com/project/FarGroup/farmanager-5lhsj/history) | TBD | TBD |
-| Clang | [![AppVeyor](https://img.shields.io/appveyor/ci/FarGroup/farmanager-tgu1s.svg?logo=appveyor)](https://ci.appveyor.com/project/FarGroup/farmanager-tgu1s/history) | TBD | TBD |
+| GCC | [![AppVeyor](https://img.shields.io/appveyor/ci/FarGroup/farmanager-5lhsj.svg?logo=appveyor)](https://ci.appveyor.com/project/FarGroup/farmanager-5lhsj/history) | [![Travis CI](https://img.shields.io/travis/FarGroup/FarManager.svg?logo=travis)](https://travis-ci.com/FarGroup/FarManager/builds) | TBD |
+| Clang | [![AppVeyor](https://img.shields.io/appveyor/ci/FarGroup/farmanager-tgu1s.svg?logo=appveyor)](https://ci.appveyor.com/project/FarGroup/farmanager-tgu1s/history) | [![Travis CI](https://img.shields.io/travis/FarGroup/FarManager.svg?logo=travis)](https://travis-ci.com/FarGroup/FarManager/builds) | TBD |
 
 
 ### Far Manager — what is it?
--- _build/ci/travis/build.cmd
+++ _build/ci/travis/build.cmd
@@ -1,40 +1,47 @@
-goto %COMPILER%
+@setlocal
+@set throw=@goto :eof
+@set exit=@goto :eof
+
+call :%COMPILER%
+
+@endlocal
+@goto :eof
 
 :MSVC_PROJ
 set PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin;%PATH%
 
 cd far
-MSBuild.exe /property:Configuration=%CONFIG% /property:Platform=%PLATFORM% far.vcxproj || exit /b
+MSBuild.exe /property:Configuration=%CONFIG% /property:Platform=%PLATFORM% far.vcxproj || %throw%
 
-goto :eof
+%exit%
 
 
 :MSVC_NMAKE
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars%PLATFORM:~-2%.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars%PLATFORM:~-2%.bat" || %throw%
 if %CONFIG%==Debug set DEBUG=1
 
 cd far
-nmake /f makefile_vc %ADD_MAKE% || exit /b
+nmake /f makefile_vc %ADD_MAKE% || %throw%
 cd ..
 
 cd plugins
-nmake /f makefile_all_vc %ADD_MAKE% || exit /b
+nmake /f makefile_all_vc %ADD_MAKE% || %throw%
 cd ..
 
-goto :eof
+%exit%
 
 
 :CLANG_NMAKE
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars%PLATFORM:~-2%.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars%PLATFORM:~-2%.bat" || %throw%
 set PATH=C:\Program Files\LLVM\bin;%PATH%
 set CLANG=1
 if %CONFIG%==Debug set DEBUG=1
 
 cd far
-nmake /f makefile_vc %ADD_MAKE% || exit /b
+nmake /f makefile_vc %ADD_MAKE% || %throw%
 cd ..
 
-goto :eof
+%exit%
 
 
 :GCC_MAKE
@@ -42,11 +49,11 @@ set PATH=C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw%PLATFORM:~-2%\b
 if %CONFIG%==Debug set DEBUG=1
 
 cd far
-mingw32-make -j4 -f makefile_gcc %ADD_MAKE% || exit /b
+mingw32-make -j4 -f makefile_gcc %ADD_MAKE% || %throw%
 cd ..
 
 cd plugins
-mingw32-make -j4 -f makefile_all_gcc %ADD_MAKE% || exit /b
+mingw32-make -j4 -f makefile_all_gcc %ADD_MAKE% || %throw%
 cd ..
 
-goto :eof
+%exit%
