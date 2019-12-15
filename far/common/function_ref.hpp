978977a17bbbb7c63d27b0479bd904fe481e5c6e
Alex Alabuzhev <alabuzhev@gmail.com>
2019-12-15 14:11:43 +0000

    More casts to make Clang happy

--- far/common/function_ref.hpp
+++ far/common/function_ref.hpp
@@ -47,8 +47,8 @@ WARNING_PUSH()
 WARNING_DISABLE_MSC(4180) // https://docs.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-level-1-c4180?view=vs-2019 qualifier applied to function type has no meaning; ignored
 	template<typename callable_type, REQUIRES(!std::is_same_v<std::decay_t<callable_type>, function_ref>)>
 	function_ref(const callable_type& Callable) noexcept:
-		m_Ptr(reinterpret_cast<const void*>(&Callable)),
-		m_ErasedFn([](const void* Ptr, args... Args) -> return_type
+		m_Ptr(const_cast<void*>(reinterpret_cast<const void*>(&Callable))),
+		m_ErasedFn([](void* Ptr, args... Args) -> return_type
 		{
 			return std::invoke(*reinterpret_cast<const callable_type*>(Ptr), FWD(Args)...);
 		})
@@ -74,9 +74,9 @@ WARNING_POP()
 	}
 
 private:
-	using signature_type = return_type(const void*, args...);
+	using signature_type = return_type(void*, args...);
 
-	const void* m_Ptr;
+	void* m_Ptr;
 	signature_type* m_ErasedFn;
 };
 
