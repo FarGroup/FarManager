bf9a2ed1ede9d057be857e718a0607284914db92
Alex Alabuzhev <alabuzhev@gmail.com>
2020-01-28 09:38:30 +0000

    Use LZMA compression for nightly builds

--- misc/nightly/publish.sh
+++ misc/nightly/publish.sh
@@ -18,8 +18,8 @@ processFarBuild()
 		return 1
 	fi
 
-	7za a -r -x!${ARCNAME}.msi -x!*.pdb ${ARCNAME}.7z *
-	7za a -r ${ARCNAME}.pdb.7z *.pdb
+	7za a -m0=LZMA -r -x!${ARCNAME}.msi -x!*.pdb ${ARCNAME}.7z *
+	7za a -m0=LZMA -r ${ARCNAME}.pdb.7z *.pdb
 
 	cd $BASE || return 1
 	m4 -P -DFARBIT=$1 -DHOSTTYPE=Unix -D ARC=../outfinalnew$1/$ARCNAME -D FARVAR=new -D LASTCHANGE="$LASTCHANGE" ../pagegen.m4 > $NIGHTLY_WEB_ROOT/FarW.$1.php
