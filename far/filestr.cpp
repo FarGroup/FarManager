b5f9055c13a60f641b6a459fc87d09895c5f3727
Alex Alabuzhev <alabuzhev@gmail.com>
2020-02-24 22:26:15 +0000

    Workaround for VS2017 ICE

--- far/filestr.cpp
+++ far/filestr.cpp
@@ -559,8 +559,10 @@ TEST_CASE("enum_lines")
 			const auto Enumerator = enum_lines(Stream, Codepage);
 
 			// Twice to make sure that reset works as expected
-			for ([[maybe_unused]] const auto n: { 0, 1 })
+			for (/*[[maybe_unused]]*/ const auto n: { 0, 1 })
 			{
+				(void)n; // [[maybe_unused]] causes ICE in VS2017. TODO: Remove after we move to 2019 or later
+
 				auto Iterator = i.Result.begin();
 
 				for (const auto& Line : Enumerator)
