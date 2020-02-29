407123230286a5e7515eb618915522fa7590874f
qlost <lost-y@yandex.ru>
2020-02-29 23:22:42 +0300

    [+] delete[] с двумя параметрами - может пригодиться в FarCalc

--- plugins/common/CRT/delete_array.cpp
+++ plugins/common/CRT/delete_array.cpp
@@ -5,4 +5,13 @@ void __cdecl operator delete[] (void *ptr)
 {
   ::operator delete(ptr);
 }
+
+#if defined(_MSC_VER) && _MSC_VER >= 1900
+void __cdecl operator delete[] (void *ptr, size_t size)
+{
+  (void)size;
+  ::operator delete(ptr);
+}
+#endif
+
 #endif
