c1b421ab2e042be59f603409d44fa48446525fc9
trexinc <trexinc@gmail.com>
2020-05-02 09:24:06 +0300

    convert to utf8

--- far/FarColorW.pas
+++ far/FarColorW.pas
@@ -5,8 +5,8 @@
 }
 
 {
-Copyright © 1996 Eugene Roshal
-Copyright © 2000 Far Group
+Copyright (c) 1996 Eugene Roshal
+Copyright (c) 2000 Far Group
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
