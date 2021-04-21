222c82f723d7a69231171b6c5ab6ba6a0a4369ba
Alex Alabuzhev <alabuzhev@gmail.com>
2021-04-21 17:18:45 +0100

    Revert "Build nightly enc.sh in _output/enc of repository root"
    This reverts commit 4f0203549aa5c5cc438827dbd33f586c675ef90b.

--- misc/nightly/enc.sh
+++ misc/nightly/enc.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# absolute path to this script
-DIR=$(dirname $(readlink -f $0))
-# absolute path to source root
-ROOT=$(cd $DIR/../.. && pwd)
-BUILDDIR=$ROOT/_output
-
-
 function benc2 {
 LNG=$1
 L=$2
@@ -15,8 +8,8 @@ cd ${LNG} || return 1
 wine "C:/Program Files (x86)/HTML Help Workshop/hhc.exe" plugins${L}.hhp
 
 ( \
-	cp -vf FarEncyclopedia.${LNG}.chm $BUILDDIR/outfinalnew32/Encyclopedia/ && \
-	cp -vf FarEncyclopedia.${LNG}.chm $BUILDDIR/outfinalnew64/Encyclopedia/ \
+	cp -f FarEncyclopedia.${LNG}.chm ../../../../outfinalnew32/Encyclopedia/ && \
+	cp -f FarEncyclopedia.${LNG}.chm ../../../../outfinalnew64/Encyclopedia/ \
 ) || return 1
 
 cd ..
@@ -32,26 +25,22 @@ wine "C:/src/enc/tools/lua/lua.exe" "C:/src/enc/tools/lua/scripts/tp2hh.lua" "..
 wine "C:/Program Files (x86)/HTML Help Workshop/hhc.exe" ${1}.hhp
 
 ( \
-	cp -f ${1}.chm $BUILDDIR/outfinalnew32/Encyclopedia/ && \
-	cp -f ${1}.chm $BUILDDIR/outfinalnew64/Encyclopedia/ \
+	cp -f ${1}.chm ../../../../outfinalnew32/Encyclopedia/ && \
+	cp -f ${1}.chm ../../../../outfinalnew64/Encyclopedia/ \
 ) || return 1
 
 cd ..
 
 }
 
+rm -fR enc
 
-echo Building in $BUILDDIR
-echo Cleaning up $BUILDDIR
-rm -fR $BUILDDIR
-mkdir $BUILDDIR
+cp -R far.git/enc ./ || exit 1
 
-cp -R $ROOT/enc $BUILDDIR/ || exit 1
+mkdir -p outfinalnew32/Encyclopedia
+mkdir -p outfinalnew64/Encyclopedia
 
-mkdir -p $BUILDDIR/outfinalnew32/Encyclopedia
-mkdir -p $BUILDDIR/outfinalnew64/Encyclopedia
-
-pushd $BUILDDIR/enc/tools || exit 1
+pushd enc/tools || exit 1
 python tool.make_chm.py
 cd ../build/chm
 
@@ -62,8 +51,8 @@ cd ../build/chm
 
 popd
 
-mkdir -p $BUILDDIR/enc/build/lua
-pushd $BUILDDIR/enc/build/lua || exit 1
+mkdir -p enc/build/lua
+pushd enc/build/lua || exit 1
 
 ( \
 	blua macroapi_manual.ru && \
@@ -74,10 +63,8 @@ pushd $BUILDDIR/enc/build/lua || exit 1
 popd
 
 #update api.farmanager.com
-pushd $BUILDDIR/enc/tools || exit 1
+pushd enc/tools || exit 1
 python ./tool.make_inet.py || exit 1
-popd
-
-#TODO this should belong to deploy script
+popd 
 rm -Rf /var/www/api/* || exit 1
-cp -Rf $BUILDDIR/enc/build/inet/* /var/www/api/ || exit 1
+cp -Rf enc/build/inet/* /var/www/api/ || exit 1
