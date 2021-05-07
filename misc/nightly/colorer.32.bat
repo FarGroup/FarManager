7044e5041791010ad245b1286f36e71ec15ebcca
Aleksey Dobrunov <ctapmex@gmail.com>
2021-05-07 23:14:25 +0500

    change colorer build to colorer download

--- misc/nightly/colorer.32.bat
+++
@@ -1,8 +0,0 @@
-call %~dp0base_32.bat
-
-set PROJECT_ROOT=%~dp0FarColorer
-set PROJECT_CONFIG=Release
-set PROJECT_CONF=x86
-
-c:\cmake\bin\cmake.exe -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=%PROJECT_CONFIG% -D COLORER_WIN64_BUILD=OFF %PROJECT_ROOT%
-nmake
