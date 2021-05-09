2370de167fdb033f9090bd2a53f613ca82192ea6
Aleksey Dobrunov <ctapmex@gmail.com>
2021-05-09 21:03:04 +0500

    fix result path; remove archive after use

--- misc/nightly/colorer.sh
+++ misc/nightly/colorer.sh
@@ -5,13 +5,16 @@ function bcolorer {
   COLORER_VERSION=1.4.1
   COLORER_PLATFORM=$2
   COLORER_FILE_NAME=FarColorer.${COLORER_PLATFORM}.v${COLORER_VERSION}.7z
+  
+  rm -f ${COLORER_FILE_NAME}
   curl -fsLJO -o ${COLORER_FILE_NAME} https://github.com/colorer/FarColorer/releases/download/v${COLORER_VERSION}/${COLORER_FILE_NAME}
   if [ ! -e ${COLORER_FILE_NAME} ]; then
     echo "Can't find ${COLORER_FILE_NAME}"
     return 1
   fi
-  mkdir ../outfinalnew${BIT}/Plugins/$PLUGIN
-  7z x ${COLORER_FILE_NAME} -o../outfinalnew${BIT}/Plugins/$PLUGIN
+  mkdir outfinalnew${BIT}/Plugins/$PLUGIN
+  7z x ${COLORER_FILE_NAME} -ooutfinalnew${BIT}/Plugins/$PLUGIN
+  rm -f ${COLORER_FILE_NAME}
 
 }
 
