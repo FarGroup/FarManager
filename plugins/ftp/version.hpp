f5f07e312f62703412e76391903486502a283c2b
Alex Alabuzhev <alabuzhev@gmail.com>
2021-09-09 00:54:47 +0100

    Continue build unification; sync resources

--- plugins/ftp/version.hpp
+++ plugins/ftp/version.hpp
@@ -3,8 +3,5 @@
 #include "farversion.hpp"
 
 #define FTP_BUILD 278
-#define FTPPRODUCTNAME "FarFtp"
-
-#define ftpgenericpluginrc(major, minor, desc, name, filename) fullgenericpluginrc_nobuild(major, minor, desc, name, filename, FARCOPYRIGHT, FAR_MAJOR_VER, FAR_MINOR_VER, FTP_BUILD, FTPPRODUCTNAME)
 
 #endif
--- plugins/multiarc/multiarcversion.hpp
+++ plugins/multiarc/multiarcversion.hpp
@@ -6,6 +6,4 @@
 #define MAPRODUCTNAME "MultiArc"
 #define FMTDESCRIPTION "Second-level plugin module for MultiArc Far Manager plugin"
 
-#define magenericpluginrc(major, minor, name, filename) fullgenericpluginrc_nobuild(major, minor, FMTDESCRIPTION, name, filename, FARCOPYRIGHT, FAR_MAJOR_VER, FAR_MINOR_VER, MA_BUILD, MAPRODUCTNAME)
-
 #endif
