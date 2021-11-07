4ff6560ab82f0acd5c28a02c155d6d6d11be7985
Alex Alabuzhev <alabuzhev@gmail.com>
2021-11-07 11:24:56 +0000

    Fix build

--- misc/nightly/installer.32.bat
+++ misc/nightly/installer.32.bat
@@ -3,7 +3,7 @@ call %~dp0base_32.bat
 SET Path=%Path%;C:\WIX
 
 set SOURCE_DIR=c:\src\outfinalnew32
-cd far && for /f "tokens=1,2,4 delims=, " %%i in ('tools\m4 -P farversion.inc.m4') do set VER_MAJOR=%%i && set VER_MINOR=%%j && set VER_BUILD=%%k && cd ..
+pushd && cd ..\..\far && for /f "tokens=1,2,4 delims=, " %%i in ('tools\m4 -P farversion.inc.m4') do set VER_MAJOR=%%i && set VER_MINOR=%%j && set VER_BUILD=%%k && popd
 set NIGHTLY=1
 
 nmake -nologo RELEASE=1 NO_LOCALISED_MSI=1
