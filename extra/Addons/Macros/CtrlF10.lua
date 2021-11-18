fbbd3baaa0f2058bae2bd0386d99396a79082c82
johndoe <johnd0e@mail.ua>
2021-11-18 18:28:06 +0200

    Improve CtrlF10.lua
    - Check if Ctrl+F10 is available in current context
    - Code formatting
    
    More info https://forum.farmanager.com/viewtopic.php?f=15&t=7812&p=150678#p150678

--- extra/Addons/Macros/CtrlF10.lua
+++ extra/Addons/Macros/CtrlF10.lua
@@ -1,12 +1,24 @@
-Macro {
-  area="Editor"; key="CtrlF10"; description="Quit from editor and position to the current file"; action = function()
-Keys('CtrlF10 Esc')
-  end;
-}
+if not panel.CheckPanelsExist() then return end -- far.exe -v -e
+
+local F6_ALLOWED, MODAL = 0x00000002, 0x00000800
+local function CtrlF10_enabled()
+  local State = Area.Viewer
+            and band(Viewer.State, MODAL)
+             -- CtrlF10 is forbidden in modal viewer
+             or band(Editor.State, bor(F6_ALLOWED,MODAL))
+             -- CtrlF10 is forbidden in modal editor,
+             -- unless it opened with explicit EF_ENABLE_F6 flag
+  return State~=MODAL
+end
 
 Macro {
-  area="Viewer"; key="CtrlF10"; description="Quit from viewer and position to the current file"; action = function()
-Keys('CtrlF10 Esc')
+  description="Quit from editor/viewer and position to the current file";
+  area="Editor Viewer";
+  key="CtrlF10";
+  flags="NoSendKeysToPlugins";
+  condition=CtrlF10_enabled;
+  action=function()
+    Keys("CtrlF10 Esc F12")
+    if Menu.Id==far.Guids.ScreensSwitchId then Keys("1") end
   end;
 }
-
