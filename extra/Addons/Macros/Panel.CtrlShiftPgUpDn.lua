66b74da4b03d0ab7ba06ef71fa62f5c14d04af6f
johndoe <johnd0e@mail.ua>
2021-11-19 18:28:17 +0200

    Improve Panel.CtrlShiftPgUpDn.lua
    - More robust
    - Jump over folders on the same level (never go to parent folder)
    - Panel.CtrlShiftPgUpDn_api.lua - additional sample
    
    Ref: https://forum.farmanager.com/viewtopic.php?f=15&t=8709

--- extra/Addons/Macros/Panel.CtrlShiftPgUpDn.lua
+++ extra/Addons/Macros/Panel.CtrlShiftPgUpDn.lua
@@ -1,24 +1,41 @@
-Macro {
-  area="Shell"; key="CtrlShiftPgUp"; description="Jump to the previous folder on the same level"; action = function()
-
-  if not APanel.Root then Keys('CtrlPgUp') end
-  if not APanel.Bof then
-    Keys('Up')
-    if APanel.Folder then Keys('CtrlPgDn') end
+local ACTIVE = 0
+local GET_ATTR = 2
+local GET_NAME = 0
+local FILE_ATTRIBUTE_DIRECTORY = 0x00000010
+local function Jump (direction)
+  local source_path,source_pos = APanel.Path,APanel.CurPos
+  Panel.SetPath(ACTIVE,"..")
+  local pos = APanel.CurPos + direction
+  if pos<=APanel.ItemCount and pos>0 then
+    local is_folder = 0~=band(Panel.Item(ACTIVE,pos,GET_ATTR),FILE_ATTRIBUTE_DIRECTORY)
+    local name = Panel.Item(ACTIVE,pos,GET_NAME)
+    if is_folder and (pos~=1 or name~="..") then
+      Panel.SetPath(ACTIVE,name)
+      return
+    end
   end
+  Panel.SetPath(ACTIVE,source_path)
+  Panel.SetPosIdx(ACTIVE,source_pos)
+end
 
+local function notRoot() return not APanel.Root end
+
+Macro {
+  description="Jump to the next folder on the same level";
+  area="Shell";
+  key="CtrlShiftPgUp CtrlShiftNum9";
+  condition=notRoot;
+  action=function()
+    Jump(-1)
   end;
 }
 
 Macro {
-  area="Shell"; key="CtrlShiftPgDn"; description="Jump to the next folder on the same level"; action = function()
-
-  if not APanel.Root then Keys('CtrlPgUp') end
-  if not APanel.Eof then
-    Keys('Down')
-    if APanel.Folder then Keys('CtrlPgDn') end
-  end
-
+  description="Jump to the previous folder on the same level";
+  area="Shell";
+  key="CtrlShiftPgDn CtrlShiftNum3";
+  condition=notRoot;
+  action=function()
+    Jump(1)
   end;
 }
-
