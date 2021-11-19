cccadb9e3221148f21826ddcce37f90cd479d6d0
johndoe <johnd0e@mail.ua>
2021-11-19 14:25:07 +0200

    Improve Panel.CtrlLeft/Right.lua
    - Combine 2 macros into 1
    - Being launched on plugin panel - the macro goes to real panel
      not changing drive
    - Pick disks only, skip plugins section
    - Does not close any warnings (original macro sent Esc)
    - Add more panel areas
    - Code formatting
    
    Ref: https://forum.farmanager.com/viewtopic.php?p=150698#p150698

--- extra/Addons/Macros/Panel.CtrlLeft.lua
+++
@@ -1,6 +0,0 @@
-Macro {
-  area="Shell"; key="CtrlLeft"; flags="EmptyCommandLine"; description="Use Ctrl-Left to change current drive to the previous"; action = function()
-Keys('F9 Enter Up Enter Up Enter Esc')
-  end;
-}
-
