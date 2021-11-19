1649810611b180b719aa8248629bcffa248e220e
johndoe <johnd0e@mail.ua>
2021-11-19 18:46:15 +0200

    Improve Panel.Esc.lua
    - Unhide both panels when one was hidden
    - Code formatting

--- extra/Addons/Macros/Panel.Esc.lua
+++ extra/Addons/Macros/Panel.Esc.lua
@@ -1,5 +1,10 @@
 Macro {
-  area="Shell Info QView Tree"; key="Esc"; flags="EmptyCommandLine"; description="Use Esc to toggle panels on/off"; action = function()
-Keys('CtrlO')
+  description="Use Esc to toggle panels on/off";
+  area="Shell Info QView Tree";
+  key="Esc";
+  flags="EmptyCommandLine";
+  action=function()
+    Keys"CtrlO"
+    if APanel.Visible~=PPanel.Visible then Keys "CtrlP" end -- unhide both panels
   end;
 }
