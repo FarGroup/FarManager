2e7bba3f14120cd41a87d38860bc5c2a0d5b32dd
johndoe <johnd0e@mail.ua>
2021-11-20 13:39:51 +0200

    Improve Panel.Numpad.lua
    - Combine into single macro

--- extra/Addons/Macros/Panel.Numpad.lua
+++ extra/Addons/Macros/Panel.Numpad.lua
@@ -1,24 +1,13 @@
-Macro {
-  area="Shell"; key="Multiply"; flags="NotEmptyCommandLine"; description="Use numpad keys in command line"; action = function()
-Keys('*')
-  end;
-}
+local Numpad = {Multiply="*", Add="+", Subtract="-", Divide="/"}
 
 Macro {
-  area="Shell"; key="Add"; flags="NotEmptyCommandLine"; description="Use numpad keys in command line"; action = function()
-Keys('+')
+  description="Use numpad keys in command line";
+  area="Shell Search";
+  key="Multiply Add Subtract Divide";
+  condition=function()
+    return not CmdLine.Empty or Area.Search
+  end;
+  action=function()
+    Keys(Numpad[mf.akey(1,1)])
   end;
 }
-
-Macro {
-  area="Shell"; key="Subtract"; flags="NotEmptyCommandLine"; description="Use numpad keys in command line"; action = function()
-Keys('-')
-  end;
-}
-
-Macro {
-  area="Shell"; key="Divide"; flags="NotEmptyCommandLine"; description="Use numpad keys in command line"; action = function()
-Keys('/')
-  end;
-}
-
