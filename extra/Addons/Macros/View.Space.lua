7895a371ba2bac6722b9c8731a5a9ec23f6cd76d
johndoe <johnd0e@mail.ua>
2021-11-20 16:42:10 +0200

    View.Space.lua: code formatting

--- extra/Addons/Macros/View.Space.lua
+++ extra/Addons/Macros/View.Space.lua
@@ -1,13 +1,13 @@
 Macro {
-  area="Viewer"; key="Space"; description="Use Space for listing in the viewer (like in Outlook Express or The Bat!)"; action = function()
-
-  if Object.Eof then
-    Keys('CtrlF10 Shift')
-    if not APanel.Eof then Keys('Add Home') end
-  else
-    Keys('PgDn')
-  end
-
+  description="Use Space for listing in the viewer (like in Outlook Express or The Bat!)";
+  area="Viewer";
+  key="Space";
+  action=function()
+    if Object.Eof then
+      Keys("CtrlF10 Shift")
+      if not APanel.Eof then Keys("Add Home") end
+    else
+      Keys("PgDn")
+    end
   end;
 }
-
