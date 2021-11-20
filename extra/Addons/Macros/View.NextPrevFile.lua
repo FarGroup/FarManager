49688db54c7c51a6644fe2152908d04302ce4046
johndoe <johnd0e@mail.ua>
2021-11-20 16:35:26 +0200

    Improve View.NextPrevFile.lua
    - Code formatting
    - Combine into single macro

--- extra/Addons/Macros/View.NextPrevFile.lua
+++ extra/Addons/Macros/View.NextPrevFile.lua
@@ -1,12 +1,7 @@
 Macro {
-  area="Viewer"; key="Add"; description="Move to next file and update panel position"; action = function()
-Keys('Add CtrlF10 Shift')
+  description="Move to next/prev file and update panel position";
+  area="Viewer"; key="Add Subtract";
+  action=function()
+    Keys("AKey CtrlF10 Shift")
   end;
 }
-
-Macro {
-  area="Viewer"; key="Subtract"; description="Move to previous file and update panel position"; action = function()
-Keys('Subtract CtrlF10 Shift')
-  end;
-}
-
