28934c6481bbb4e398dbcf233dbb7b21fedf3cc0
Vladimir Surguchev <vladimir.surguchev@gmail.com>
2021-11-30 18:22:07 +0300

    7z 21.06

--- plugins/arclite/version.hpp
+++ plugins/arclite/version.hpp
@@ -1,6 +1,6 @@
 #include <farversion.hpp>
 
-#define PLUGIN_BUILD 321
+#define PLUGIN_BUILD 322
 #define PLUGIN_DESC L"Archive support for Far Manager (based on 7-Zip project)"
 #define PLUGIN_NAME L"ArcLite"
 #define PLUGIN_FILENAME L"arclite.dll"
