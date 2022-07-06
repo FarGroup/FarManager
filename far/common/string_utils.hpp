a5ec748c19c03721d7c7390fbb20076998fb112b
Alex Alabuzhev <alabuzhev@gmail.com>
2022-07-06 23:23:29 +0100

    Build vc_crt_fix once; ensure that common headers are compilable in isolation

--- far/common/string_utils.hpp
+++ far/common/string_utils.hpp
@@ -37,6 +37,7 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "type_traits.hpp"
 #include "utility.hpp"
 
+#include <array>
 #include <numeric>
 #include <string>
 #include <string_view>
