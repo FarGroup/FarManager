107f1343e74c2e1e661b4a979233b24fa0f2d502
Alex Alabuzhev <alabuzhev@gmail.com>
2023-06-26 08:50:23 +0000

    Revert "Improve Panel.SpaceToSelect.lua"
    This reverts commit 18aa066d6577d827e05e6cc6f00ca55e7c7906eb.

--- extra/Addons/Macros/Panel.SpaceToSelect.lua
+++ extra/Addons/Macros/Panel.SpaceToSelect.lua
@@ -1,4 +1,3 @@
-local APANEL,INVERT,MODE_IDX,CUR_ITEM = 0,2,1,0
 Macro {
   description="Panel: Use Space to select files";
   area="Shell";
@@ -7,6 +6,6 @@ Macro {
     return CmdLine.Empty and APanel.Visible
   end;
   action=function()
-    Panel.Select(APANEL,INVERT,MODE_IDX,CUR_ITEM)
+    Keys('Ins')
   end;
 }
