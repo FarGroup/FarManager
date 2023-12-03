5c9bce3ac7bb97fbd81c40d1c8e4c9ce35ac7d84
Shmuel Zeigerman <solomuz0@gmail.com>
2023-12-03 23:56:15 +0200

    Correct MinFarVersion for LuaMacro

--- plugins/luamacro/_globalinfo.lua
+++ plugins/luamacro/_globalinfo.lua
@@ -1,7 +1,7 @@
 function export.GetGlobalInfo()
   return {
     Version       = { 3, 0, 0, 794 },
-    MinFarVersion = { 3, 0, 0, 5171 },
+    MinFarVersion = { 3, 0, 0, 6214 },
     Guid          = win.Uuid("4EBBEFC8-2084-4B7F-94C0-692CE136894D"),
     Title         = "LuaMacro",
     Description   = "Far macros in Lua",
