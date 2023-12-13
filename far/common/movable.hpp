f20ec3139517f586ea15433abc9e8e41092aa57f
Alex Alabuzhev <alabuzhev@gmail.com>
2023-12-13 23:07:02 +0000

    Add missing include

--- far/common/movable.hpp
+++ far/common/movable.hpp
@@ -39,6 +39,7 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "preprocessor.hpp"
 
 #include <iterator>
+#include <utility>
 
 //----------------------------------------------------------------------------
 
