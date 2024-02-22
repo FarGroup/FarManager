d3d5b7224e62afb8d49c7fa8a9011ba486d21259
Alex Alabuzhev <alabuzhev@gmail.com>
2024-02-22 19:23:39 +0000

    Source NetBox from origin
    Adjust names

--- misc/nightly/netbox.sh
+++ misc/nightly/netbox.sh
@@ -3,15 +3,22 @@
 function bnetbox {
   BIT=$1
   PLUGIN=NetBox
+  NETBOX_PLATFORM=$2
   NETBOX_VERSION=$(curl --silent "https://api.github.com/repos/michaellukashov/Far-NetBox/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
+  NETBOX_FILE_VERSION=$(curl --silent "https://api.github.com/repos/michaellukashov/Far-NetBox/releases/latest" | grep -E '"browser_download_url": ".+'${NETBOX_PLATFORM}'.+"'| grep -v pdb | sed -E 's/.+NetBox\.'${NETBOX_PLATFORM}'\.(.+)\.7z.+/\1/')
+
   if [ -z "$NETBOX_VERSION" ]; then
     echo "Failed to get NetBox version"
     return 1
   fi
 
-  echo "Download NetBox ${NETBOX_VERSION}"
-  NETBOX_PLATFORM=$2
-  NETBOX_BASE_NAME=NetBox.${NETBOX_PLATFORM}.${NETBOX_VERSION}
+  if [ -z "NETBOX_FILE_VERSION" ]; then
+    echo "Failed to get NetBox file version"
+    return 1
+  fi
+
+  echo "Download NetBox ${NETBOX_FILE_VERSION}"
+  NETBOX_BASE_NAME=NetBox.${NETBOX_PLATFORM}.${NETBOX_FILE_VERSION}
   NETBOX_FILE_NAME=${NETBOX_BASE_NAME}.7z
   NETBOX_PDB_NAME=${NETBOX_BASE_NAME}.pdb.7z
 
