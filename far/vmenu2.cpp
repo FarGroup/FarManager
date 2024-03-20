753e6d9742ff2d548d35a62e6936471cc700dfdf
Alex Alabuzhev <alabuzhev@gmail.com>
2024-03-20 23:16:00 +0000

    Partial revert of gh-811 due to M#4035

--- far/vmenu2.cpp
+++ far/vmenu2.cpp
@@ -152,6 +152,9 @@ intptr_t VMenu2::VMenu2DlgProc(Dialog* Dlg, intptr_t Msg, intptr_t Param1, void*
 		break;
 
 	case DN_LISTCHANGE:
+		if (Dlg->CheckDialogMode(DMODE_ISMENU))
+			break;
+
 		if(!cancel)
 		{
 			if(Call(Msg, Param2))
