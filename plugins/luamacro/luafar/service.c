9652786836641ec396f70303a99939a10ab7efdb
Shmuel Zeigerman <solomuz0@gmail.com>
2024-04-03 00:07:33 +0300

    LuaFAR: DM_SETDLGDATA returns the previous value (was: returns nothing)

--- plugins/luamacro/luafar/service.c
+++ plugins/luamacro/luafar/service.c
@@ -3219,9 +3219,10 @@ static int DoSendDlgMessage (lua_State *L, intptr_t Msg, int delta)
 
 		case DM_SETDLGDATA: {
 			TDialogData *dd = (TDialogData*) Info->SendDlgMessage(hDlg,DM_GETDLGDATA,0,0);
-			lua_settop(L, pos3);
+			lua_rawgeti(L, LUA_REGISTRYINDEX, dd->dataRef);
+			lua_pushvalue(L, pos3);
 			lua_rawseti(L, LUA_REGISTRYINDEX, dd->dataRef);
-			return 0;
+			return 1;
 		}
 
 		case DM_GETDLGRECT:
--- plugins/luamacro/luafar/version.h
+++ plugins/luamacro/luafar/version.h
@@ -1,3 +1,3 @@
 ﻿#include <farversion.hpp>
 
-#define PLUGIN_BUILD 816
+#define PLUGIN_BUILD 817
