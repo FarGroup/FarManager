8d1fc4d68092498130f02772df328a1aca279326
Shmuel Zeigerman <solomuz0@gmail.com>
2024-07-13 15:21:49 +0300

    LuaMacro: a small fix

--- plugins/luamacro/luafar/version.h
+++ plugins/luamacro/luafar/version.h
@@ -1,3 +1,3 @@
 ﻿#include <farversion.hpp>
 
-#define PLUGIN_BUILD 845
+#define PLUGIN_BUILD 846
--- plugins/luamacro/luamacro.lua
+++ plugins/luamacro/luamacro.lua
@@ -514,7 +514,7 @@ end
 -- TODO: when called from a module's panel, call that module's Configure()
 function export.Configure (guid)
   local items = utils.GetMenuItems()
-  if items[guid] then items[guid].action() end
+  if items[guid] then items[guid].action(guid) end
 end
 
 local function Init()
