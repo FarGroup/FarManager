ab4625097e58691fe6e22c83b6a923eb2df54441
Alex Alabuzhev <alabuzhev@gmail.com>
2024-09-14 09:49:49 +0100

    Fix build

--- misc/nightly/plugin.64.bat
+++ misc/nightly/plugin.64.bat
@@ -1,3 +1,3 @@
 call %~dp0base_64.bat
 
-nmake /f makefile_vc FAR_WORKDIR=..\..\outfinalnew64 BUILD=1 LUA=luasdk\20240301\64\lua.exe
+nmake /f makefile_vc FAR_WORKDIR=..\..\outfinalnew64 BUILD=1 LUA=luasdk\20240911\64\lua.exe
