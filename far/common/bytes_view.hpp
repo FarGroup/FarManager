410beeb8233550d676ba0d57be5d75197c93bf52
Alex Alabuzhev <alabuzhev@gmail.com>
2024-09-22 22:27:49 +0100

    Fix libc++ build

--- far/common/bytes_view.hpp
+++ far/common/bytes_view.hpp
@@ -41,6 +41,101 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 namespace detail
 {
+	template<typename type, typename base_type>
+	struct same_traits: std::char_traits<base_type>
+	{
+	private:
+		using base = std::char_traits<base_type>;
+
+		static constexpr auto* to(type* Char)       { return std::bit_cast<base_type*>(Char); }
+		static constexpr auto* to(const type* Char) { return std::bit_cast<const base_type*>(Char); }
+		static constexpr auto& to(type& Char)       { return *to(&Char); }
+		static constexpr auto& to(const type& Char) { return *to(&Char); }
+
+		static constexpr auto* from(base_type* Char)       { return std::bit_cast<type*>(Char); }
+		static constexpr auto* from(const base_type* Char) { return std::bit_cast<const type*>(Char); }
+		static constexpr auto& from(base_type& Char)       { return *from(&Char); }
+		static constexpr auto& from(const base_type& Char) { return *from(&Char); }
+
+	public:
+		using char_type  = type;
+		using int_type   = typename base::int_type;
+		using off_type   = typename base::off_type;
+		using pos_type   = typename base::pos_type;
+		using state_type = typename base::state_type;
+
+		static void assign(char_type& Left, const char_type& Right) noexcept
+		{
+			return base::assign(to(Left), to(Right));
+		}
+
+		static char_type* assign(char_type* const Data, size_t const Size, char_type const Value) noexcept
+		{
+			return from(base::assign(to(Data), Size, to(Value)));
+		}
+
+		static bool eq(char_type const Left, char_type const Right)
+		{
+			return base::eq(to(Left), to(Right));
+		}
+
+		static bool lt(char_type const Left, char_type const Right)
+		{
+			return base::lt(to(Left), to(Right));
+		}
+
+		static int compare(const char_type* const Str1, const char_type* const Str2, size_t const Size)
+		{
+			return base::compare(to(Str1), to(Str2), Size);
+		}
+
+		static size_t length(const char_type* const Data)
+		{
+			return base::length(to(Data));
+		}
+
+		static const char_type* find(const char_type* const Data, size_t const Size, const char_type& Value)
+		{
+			return from(base::find(to(Data), Size, to(Value)));
+		}
+
+		static char_type* move(char_type* const Dst, const char_type* const Src, std::size_t const Size)
+		{
+			return from(base::move(to(Dst), to(Src), Size));
+		}
+
+		static char_type* copy(char_type* const Dst, const char_type* const Src, std::size_t const Size)
+		{
+			return from(base::copy(to(Dst), to(Src), Size));
+		}
+
+		static int_type not_eof(int_type const Value)
+		{
+			return base::not_eof(Value);
+		}
+
+		static char_type to_char_type(int_type const Value)
+		{
+			return from(base::to_char_type(Value));
+		}
+
+		static int_type to_int_type(char_type const Value)
+		{
+			return base::to_int_type(to(Value));
+		}
+
+		static bool eq_int_type(int_type a, int_type b)
+		{
+			return base::eq_int_type(a, b);
+		}
+
+		static int_type eof()
+		{
+			return base::eof();
+		}
+
+	};
+
 	template<typename T, bool Add>
 	using add_const_if_t = std::conditional_t<Add, T const, T>;
 
@@ -73,8 +168,9 @@ namespace detail
 	}
 }
 
+using byte_traits = detail::same_traits<std::byte, char>;
 
-using bytes_view = std::basic_string_view<std::byte>;
+using bytes_view = std::basic_string_view<std::byte, byte_traits>;
 
 [[nodiscard]]
 constexpr bytes_view operator""_bv(const char* Str, std::size_t Size) noexcept
@@ -83,7 +179,7 @@ constexpr bytes_view operator""_bv(const char* Str, std::size_t Size) noexcept
 }
 
 
-using bytes = std::basic_string<std::byte>;
+using bytes = std::basic_string<std::byte, byte_traits>;
 
 [[nodiscard]]
 /*constexpr*/ inline bytes operator""_b(const char* Str, std::size_t Size) noexcept
