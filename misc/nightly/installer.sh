663920b6c57c24cd45b02b6db78de23472fb0c87
Alex Alabuzhev <alabuzhev@gmail.com>
2025-01-20 02:07:56 +0000

    Fix paths

--- misc/nightly/installer.sh
+++ misc/nightly/installer.sh
@@ -2,13 +2,13 @@
 
 cd misc/msi-installer || exit 1
 
-python far/tools/stacktrace.py outfinalnew32
+python ../../far/tools/stacktrace.py ../../outfinalnew32
 WINEDLLOVERRIDES="msi=n" wine cmd /c ../../installer.32.bat
 
-python far/tools/stacktrace.py outfinalnew64
+python ../../far/tools/stacktrace.py ../../outfinalnew64
 WINEDLLOVERRIDES="msi=n" wine cmd /c ../../installer.64.bat
 
-python far/tools/stacktrace.py outfinalnewARM64
+python ../../far/tools/stacktrace.py ../../outfinalnewARM64
 WINEDLLOVERRIDES="msi=n" wine cmd /c ../../installer.ARM64.bat
 
 cd ../..
