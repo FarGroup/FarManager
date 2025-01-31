85dda882348a3b60659de06b811580c3ae2b33b0
Alex Alabuzhev <alabuzhev@gmail.com>
2025-01-31 20:47:44 +0000

    Language menu fixes

--- far/config.cpp
+++ far/config.cpp
@@ -3362,7 +3362,14 @@ void Options::ShellOptions(bool LastCommand, const MOUSE_EVENT_RECORD *MouseEven
 				break;
 			case MENU_OPTIONS_LANGUAGES:   // Languages
 				{
-					if (const auto InterfaceLanguage = SelectInterfaceLanguage(strLanguage); !InterfaceLanguage.empty())
+					const auto InterfaceLanguage = SelectInterfaceLanguage(strLanguage);
+
+					// User pressed Esc, quit
+					if (InterfaceLanguage.empty())
+						break;
+
+					// Try to load only if changed
+					if (InterfaceLanguage != strLanguage.Get())
 					{
 						try
 						{
@@ -3379,17 +3386,27 @@ void Options::ShellOptions(bool LastCommand, const MOUSE_EVENT_RECORD *MouseEven
 								{ lng::MOk });
 						}
 
-						if (const auto HelpLanguage = SelectHelpLanguage(strHelpLanguage); !HelpLanguage.empty())
+						// Update only if loaded
+						if (strLanguage.Get() == InterfaceLanguage)
 						{
-							strHelpLanguage = HelpLanguage;
+							Global->CtrlObject->Plugins->ReloadLanguage();
+							os::env::set(L"FARLANG"sv, strLanguage);
+							PrepareUnitStr();
+							Global->WindowManager->InitKeyBar();
+							Global->CtrlObject->Cp()->RedrawKeyBar();
+							Global->CtrlObject->Cp()->SetScreenPosition();
 						}
-						Global->CtrlObject->Plugins->ReloadLanguage();
-						os::env::set(L"FARLANG"sv, strLanguage);
-						PrepareUnitStr();
-						Global->WindowManager->InitKeyBar();
-						Global->CtrlObject->Cp()->RedrawKeyBar();
-						Global->CtrlObject->Cp()->SetScreenPosition();
 					}
+
+					const auto HelpLanguage = SelectHelpLanguage(strHelpLanguage);
+
+					// User pressed Esc, quit
+					if (HelpLanguage.empty())
+						break;
+
+					// Try to load only if changed
+					if (HelpLanguage != strHelpLanguage.Get())
+						strHelpLanguage = HelpLanguage;
 
 					break;
 				}
