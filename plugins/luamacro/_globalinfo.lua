887aa708f18e0fd187343b542c1e762f0c9a3ccd
Shmuel Zeigerman <solomuz0@gmail.com>
2025-04-17 11:19:22 +0300

    Correction

--- plugins/luamacro/_globalinfo.lua
+++ plugins/luamacro/_globalinfo.lua
@@ -1,7 +1,7 @@
 function export.GetGlobalInfo()
   return {
     Version       = { 3, 0, 0, 873 },
-    MinFarVersion = { 3, 0, 0, 6422 },
+    MinFarVersion = { 3, 0, 0, 6462 },
     Guid          = win.Uuid("4EBBEFC8-2084-4B7F-94C0-692CE136894D"),
     Title         = "LuaMacro",
     Description   = "Far macros in Lua",
