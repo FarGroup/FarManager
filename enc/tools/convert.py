55b9e6bb8c06c67c8dd7c7c1d08ef30419577e41
Alex Alabuzhev <alabuzhev@gmail.com>
2025-07-27 21:24:21 +0100

    Use local encyclopedia in Lua chms

--- enc/tools/convert.py
+++ enc/tools/convert.py
@@ -2,11 +2,24 @@
 
 import sys
 
+def postprocess(data):
+	tokens = {
+		"ENC_URL": "ms-its:FarEncyclopedia.ru.chm::/html"
+	}
+
+	for (k, v) in tokens.items():
+		data = data.replace(f"$({k})", v)
+
+	return data
+
+
 def convert(from_filename, from_encoding, to_filename, to_encoding):
 	data = None
 
 	with open(from_filename, encoding=from_encoding) as in_file:
 		data = in_file.read()
+
+	data = postprocess(data)
 
 	with open(to_filename, "w", encoding=to_encoding) as out_file:
 		out_file.write(data)
