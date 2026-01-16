026a9227f7527fee78b8fb68aa3125087dd7529e
Vadim Yegorov <zg@bmg.lv>
2026-01-16 16:00:12 +0200

    Update headers to 6632

--- plugins/common/unicode/farcolor.hpp
+++ plugins/common/unicode/farcolor.hpp
@@ -5,7 +5,7 @@
 /*
 farcolor.hpp
 
-Colors Index for FAR Manager 3.0.6606.0
+Colors Index for FAR Manager 3.0.6632.0
 */
 /*
 Copyright © 1996 Eugene Roshal
--- plugins/common/unicode/plugin.hpp
+++ plugins/common/unicode/plugin.hpp
@@ -6,7 +6,7 @@
 /*
 plugin.hpp
 
-Plugin API for Far Manager 3.0.6606.0
+Plugin API for Far Manager 3.0.6632.0
 */
 /*
 Copyright © 1996 Eugene Roshal
@@ -44,7 +44,7 @@ other possible license with no implications from the above license on them.
 #define FARMANAGERVERSION_MAJOR 3
 #define FARMANAGERVERSION_MINOR 0
 #define FARMANAGERVERSION_REVISION 0
-#define FARMANAGERVERSION_BUILD 6606
+#define FARMANAGERVERSION_BUILD 6632
 #define FARMANAGERVERSION_STAGE VS_PRIVATE
 
 #ifndef RC_INVOKED
@@ -1879,6 +1879,8 @@ struct EditorInfo
 	size_t SessionBookmarkCount;
 	uintptr_t CurState;
 	uintptr_t CodePage;
+	RECT WindowArea;
+	RECT ClientArea;
 };
 
 struct EditorBookmarks
